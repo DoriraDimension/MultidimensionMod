ee7c94e8f22e13d57eddbeb5c93068c21ce30ab6
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2021-11-02 23:08:03 +0100

    Gila Elemental

---
+++ NPCs/Desert/GilaMonster.cs
@@ -0,0 +1,76 @@
+﻿using MultidimensionMod.Items.Banners;
+using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.NPCs.Desert
+{
+	public class GilaMonster : ModNPC
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Gilania");
+			DisplayName.AddTranslation(GameCulture.German, "Gilania");
+			Main.npcFrameCount[npc.type] = 6;
+		}
+
+		public override void SetDefaults()
+		{
+			npc.CloneDefaults(NPCID.Crab);
+			npc.width = 75;
+			npc.height = 70;
+			npc.damage = 26;
+			npc.defense = 20;
+			npc.lifeMax = 210;
+			npc.HitSound = SoundID.NPCHit1;
+			npc.DeathSound = SoundID.NPCDeath1;
+			npc.value = 60f;
+			npc.knockBackResist = 0.5f;
+			npc.aiStyle = 3;
+			aiType = NPCID.Crab;
+			animationType = NPCID.Squirrel;
+			banner = npc.type;
+			bannerItem = ModContent.ItemType<GilaBanner>();
+		}
+
+		public override void NPCLoot()
+		{
+			if (Main.rand.NextFloat() < .8000f)
+			{
+				Item.NewItem(npc.getRect(), ItemID.VialofVenom);
+			}
+		}
+
+		public override void OnHitPlayer(Player player, int damage, bool crit)
+		{
+		     player.AddBuff(BuffID.Venom, 240);
+
+		}
+
+		public override float SpawnChance(NPCSpawnInfo spawnInfo)
+		{
+			if (spawnInfo.player.ZoneDesert && Main.hardMode && Main.dayTime && spawnInfo.spawnTileY <= Main.maxTilesY - 200 && spawnInfo.spawnTileY > Main.rockLayer)
+			{
+				return 0.15f;
+			}
+			else if (spawnInfo.player.ZoneDesert && Main.hardMode && !Main.dayTime && !spawnInfo.player.ZoneSandstorm)
+			{
+				return 0.15f;
+			}
+			return 0f;
+		}
+
+		public override void HitEffect(int hitDirection, double damage)
+		{
+			if (npc.life <= 0)
+			{
+				Gore.NewGore(npc.position, npc.velocity, mod.GetGoreSlot("Gores/Desert/GilaGore1"), npc.scale);
+				Gore.NewGore(npc.position, npc.velocity, mod.GetGoreSlot("Gores/Desert/GilaGore2"), npc.scale);
+				Gore.NewGore(npc.position, npc.velocity, mod.GetGoreSlot("Gores/Desert/GilaGore3"), npc.scale);
+			}
+		}
+	}
+}
---
+++ NPCs/Desert/LesserSandElemental.cs
@@ -0,0 +1,61 @@
+﻿using MultidimensionMod.Items.Banners;
+using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.NPCs.Desert
+{
+	public class LesserSandElemental : ModNPC
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Lesser Sand Elemental");
+			DisplayName.AddTranslation(GameCulture.German, "Niederer Sand Elementar");
+			Main.npcFrameCount[npc.type] = 5;
+		}
+
+		public override void SetDefaults()
+		{
+			npc.CloneDefaults(NPCID.FlyingSnake);
+			npc.width = 36;
+			npc.height = 36;
+			npc.damage = 28;
+			npc.defense = 7;
+			npc.lifeMax = 60;
+			npc.HitSound = SoundID.NPCHit23;
+			npc.DeathSound = SoundID.NPCDeath6;
+			npc.value = 60f;
+			npc.knockBackResist = 0.3f;
+			npc.aiStyle = 44;
+			aiType = NPCID.FlyingSnake;
+			animationType = NPCID.Bird;
+			banner = npc.type;
+			bannerItem = ModContent.ItemType<LesserSandEleBanner>();
+		}
+
+		public override void NPCLoot()
+		{
+			Item.NewItem(npc.getRect(), mod.ItemType("ManaInfusedSandstone"), Main.rand.Next(1, 3));
+		}
+
+		public override float SpawnChance(NPCSpawnInfo spawnInfo)
+		{
+			if (spawnInfo.player.ZoneDesert && spawnInfo.spawnTileY <= Main.maxTilesY - 200 && spawnInfo.spawnTileY > Main.rockLayer)
+			{
+				return 0.05f;
+			}
+			return 0f;
+		}
+
+		public override void HitEffect(int hitDirection, double damage)
+		{
+			if (npc.life <= 0)
+			{
+				Gore.NewGore(npc.position, npc.velocity, mod.GetGoreSlot("Gores/Desert/SandEleGore"), npc.scale);
+			}
+		}
+	}
+}
