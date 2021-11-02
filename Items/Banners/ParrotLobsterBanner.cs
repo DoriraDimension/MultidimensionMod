34b98041a3228b793e608d429d71d6c00768c522
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2021-11-02 22:39:26 +0100

    Squished animals flat and made them into banners

---
+++ Items/Banners/ParrotLobsterBanner.cs
@@ -0,0 +1,27 @@
+﻿using MultidimensionMod.Tiles;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace MultidimensionMod.Items.Banners
+{
+	public class ParrotLobsterBanner : ModItem
+	{
+		public override void SetDefaults()
+		{
+			item.width = 12;
+			item.height = 28;
+			item.maxStack = 99;
+			item.useTurn = true;
+			item.autoReuse = true;
+			item.useAnimation = 15;
+			item.useTime = 10;
+			item.useStyle = ItemUseStyleID.SwingThrow;
+			item.consumable = true;
+			item.rare = ItemRarityID.Blue;
+			item.value = Item.sellPrice(silver: 3);
+			item.createTile = ModContent.TileType<MonsterBanner>();
+			item.placeStyle = 5;
+		}
+	}
+}
