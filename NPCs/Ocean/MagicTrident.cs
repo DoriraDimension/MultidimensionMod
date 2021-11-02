fb0c473667534a02e9259d87161f5e42456a9bfc
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2021-11-02 23:09:00 +0100

    Ocean Deluxe

---
+++ NPCs/Ocean/MagicTrident.cs
@@ -0,0 +1,56 @@
+﻿using MultidimensionMod.Items.Banners;
+using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.NPCs.Ocean
+{
+	public class MagicTrident : ModNPC
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Magic Trident");
+			DisplayName.AddTranslation(GameCulture.German, "Magischer Dreizack");
+			Main.npcFrameCount[npc.type] = Main.npcFrameCount[NPCID.EnchantedSword];
+		}
+
+		public override void SetDefaults()
+		{
+			npc.width = 80;
+			npc.height = 80;
+			npc.damage = 85;
+			npc.defense = 12;
+			npc.lifeMax = 230;
+			npc.HitSound = SoundID.NPCHit4;
+			npc.DeathSound = SoundID.NPCDeath6;
+			npc.value = 60f;
+			npc.knockBackResist = 0.6f;
+			npc.aiStyle = 23;
+			aiType = NPCID.EnchantedSword;
+			animationType = NPCID.EnchantedSword;
+			banner = npc.type;
+			bannerItem = ModContent.ItemType<TridentBanner>();
+		}
+
+		public override void NPCLoot()
+		{
+			if (Main.rand.NextFloat() < .0100f)
+			{
+				Item.NewItem(npc.getRect(), ItemID.Nazar);
+			}
+
+			if (Main.rand.NextFloat() < .0400f)
+			{
+				Item.NewItem(npc.getRect(), mod.ItemType("OldSeaCrown"));
+			}
+		}
+
+		public override float SpawnChance(NPCSpawnInfo spawnInfo)
+		{
+			return !spawnInfo.playerSafe && Main.hardMode ? SpawnCondition.Ocean.Chance * 0.18f : 0f;
+		}
+	}
+}
---
+++ NPCs/Ocean/StormFrontEel.cs
@@ -0,0 +1,71 @@
+﻿using MultidimensionMod.Items.Banners;
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.NPCs.Ocean
+{
+	public class StormFrontEel : ModNPC
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Storm Front Eel");
+			DisplayName.AddTranslation(GameCulture.German, "Sturmfront Aal");
+			Main.npcFrameCount[npc.type] = 5;
+		}
+
+		public override void SetDefaults()
+		{
+			npc.CloneDefaults(NPCID.DungeonSpirit);
+			npc.width = 80;
+			npc.height = 80;
+			npc.damage = 85;
+			npc.defense = 30;
+			npc.lifeMax = 2000;
+			npc.HitSound = SoundID.NPCHit9;
+			npc.DeathSound = SoundID.NPCDeath33;
+			npc.value = 60f;
+			npc.knockBackResist = 0.3f;
+			npc.aiStyle = 56;
+			aiType = NPCID.DungeonSpirit;
+			animationType = NPCID.Bird;
+			banner = npc.type;
+			bannerItem = ModContent.ItemType<StormEelBanner>();
+		}
+
+		public override void NPCLoot()
+		{
+			if (Main.rand.NextFloat() < .5000f)
+			{
+				Item.NewItem(npc.getRect(), mod.ItemType("StormHide"));
+			}
+
+			if (Main.rand.NextFloat() < .2000f)
+			{
+				Item.NewItem(npc.getRect(), mod.ItemType("ThunderbubbleBow"));
+			}
+
+			Item.NewItem(npc.getRect(), mod.ItemType("TidalQuartz"), Main.rand.Next(1, 4));
+		}
+
+		public override float SpawnChance(NPCSpawnInfo spawnInfo)
+		{
+			return !spawnInfo.playerSafe && Main.hardMode && Main.raining && NPC.downedGolemBoss && !NPC.AnyNPCs(ModContent.NPCType<StormFrontEel>()) ? SpawnCondition.Ocean.Chance * 0.15f : 0f;
+		}
+
+		public override void HitEffect(int hitDirection, double damage)
+		{
+			if (npc.life <= 0)
+			{
+				Gore.NewGore(npc.position, npc.velocity, mod.GetGoreSlot("Gores/Ocean/StormEelGore1"), npc.scale);
+				Gore.NewGore(npc.position, npc.velocity, mod.GetGoreSlot("Gores/Ocean/StormEelGore2"), npc.scale);
+				Gore.NewGore(npc.position, npc.velocity, mod.GetGoreSlot("Gores/Ocean/StormEelGore3"), npc.scale);
+			}
+		}
+	}
+}
