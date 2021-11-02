5fa639c13fd04d949c9eb126863fc0b5f87b982e
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2021-11-02 22:54:55 +0100

    Worms, Shark and... It?

---
+++ Items/Weapons/Ranged/Guns/MetalWormGun.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using MultidimensionMod.Items.Materials;
+using MultidimensionMod.Projectiles.Ranged;
+using Terraria;
+using Microsoft.Xna.Framework;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Items.Weapons.Ranged.Guns
+{
+	public class MetalWormGun : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Handheld Destroyer");
+			Tooltip.SetDefault("The rotten flesh fell off and it evolved to take on the place of the Destroyer.");
+			DisplayName.AddTranslation(GameCulture.German, "Handheld Zerstörer");
+			Tooltip.AddTranslation(GameCulture.German, "Das ferfaulte Fleisch ist abgefallen und es entwickelte sich um den Platz des Zerstörers einzunehmen.");
+		}
+
+		public override void SetDefaults()
+		{
+			item.damage = 89;
+			item.ranged = true;
+			item.width = 68;
+			item.height = 22;
+			item.useTime = 42;
+			item.useAnimation = 42;
+			item.useStyle = ItemUseStyleID.HoldingOut;
+			item.noMelee = true;
+			item.knockBack = 4;
+			item.value = Item.sellPrice(gold: 3);
+			item.rare = ItemRarityID.Pink;
+			item.UseSound = SoundID.Item33;
+			item.autoReuse = true;
+			item.shoot = 10;
+			item.shootSpeed = 30f;
+			item.useAmmo = AmmoID.Bullet;
+			item.crit = 15;
+		}
+
+		public override void ModifyTooltips(List<TooltipLine> list)
+		{
+			foreach (TooltipLine item in list)
+			{
+				if (item.mod == "Terraria" && item.Name == "ItemName")
+				{
+					item.overrideColor = MDRarity.RelicWeapon;
+				}
+			}
+		}
+
+		public override Vector2? HoldoutOffset()
+		{
+			return new Vector2(-10, 0);
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ModContent.ItemType<SnipeWorm>());
+			recipe.AddIngredient(ItemID.SoulofMight, 12);
+			recipe.AddTile(134);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+
+		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+		{
+			if (type == ProjectileID.Bullet)
+			{
+				type = ModContent.ProjectileType<DestroyerDualLaser>();
+			}
+			return true;
+		}
+	}
+}
---
+++ Items/Weapons/Ranged/Guns/SnipeWorm.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using MultidimensionMod.Items.Materials;
+using Terraria;
+using Microsoft.Xna.Framework;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Items.Weapons.Ranged.Guns
+{
+	public class SnipeWorm : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Snipe Worm");
+			Tooltip.SetDefault("A sniper rifle that was restored from old relics, it has rotten skin around its metal.");
+			DisplayName.AddTranslation(GameCulture.German, "Snipe Wurm");
+			Tooltip.AddTranslation(GameCulture.German, "Ein Snipergewehr das aus alten Relikten restauriert wurde, es hat verottetes Fleisch um seine Metall.");
+		}
+
+		public override void SetDefaults()
+		{
+			item.damage = 54;
+			item.ranged = true;
+			item.width = 64;
+			item.height = 22;
+			item.useTime = 46;
+			item.useAnimation = 46;
+			item.useStyle = ItemUseStyleID.HoldingOut;
+			item.noMelee = true;
+			item.knockBack = 4;
+			item.value = Item.sellPrice(silver: 23);
+			item.rare = ItemRarityID.Green;
+			item.UseSound = SoundID.Item11;
+			item.autoReuse = true;
+			item.shoot = 10;
+			item.shootSpeed = 30f;
+			item.useAmmo = AmmoID.Bullet;
+			item.crit = 10;
+		}
+
+		public override void ModifyTooltips(List<TooltipLine> list)
+		{
+			foreach (TooltipLine item in list)
+			{
+				if (item.mod == "Terraria" && item.Name == "ItemName")
+				{
+					item.overrideColor = MDRarity.RelicWeapon;
+				}
+			}
+		}
+
+		public override Vector2? HoldoutOffset()
+		{
+			return new Vector2(-10, 0);
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ModContent.ItemType<OldTaintedMandible>());
+			recipe.AddIngredient(ModContent.ItemType<OldTaintedCurseContainer>());
+			recipe.AddIngredient(ItemID.ShadowScale, 20);
+			recipe.AddTile(377);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+
+		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+		{
+			if (type == ProjectileID.Bullet)
+			{
+				type = ProjectileID.CursedBullet;
+			}
+			return true;
+		}
+	}
+}
