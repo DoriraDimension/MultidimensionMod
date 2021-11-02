a30b4756d4282a455dbe8c3795a3058545f3d2f8
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2021-11-02 18:21:16 +0100

    Shit minion of trash

---
+++ Buffs/Minions/DarkRebelsBuff.cs
@@ -0,0 +1,36 @@
+﻿using MultidimensionMod.Projectiles.Minions;
+using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Buffs.Minions
+{
+	public class DarkRebelsBuff : ModBuff
+	{
+		public override void SetDefaults()
+		{
+			DisplayName.SetDefault("Dark Rebel");
+			Description.SetDefault("One of Smiley's trusted soldiers helps you in battle");
+			DisplayName.AddTranslation(GameCulture.German, "Dunkle Rebell");
+			Description.AddTranslation(GameCulture.German, "Einer von Smiley's vertrauten Soldaten hilft dir im Kampf.");
+			Main.buffNoSave[Type] = true;
+			Main.buffNoTimeDisplay[Type] = true;
+		}
+
+		public override void Update(Player player, ref int buffIndex)
+		{
+			if (player.ownedProjectileCounts[ModContent.ProjectileType<DarkRebelsMinion>()] > 0)
+			{
+				player.buffTime[buffIndex] = 18000;
+			}
+			else
+			{
+				player.DelBuff(buffIndex);
+				buffIndex--;
+			}
+		}
+	}
+}
