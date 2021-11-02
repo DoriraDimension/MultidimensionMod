6e6d554a80442a03e701981d3587d8ca24822ae6
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2021-11-02 18:14:25 +0100

    FIRE BURNS I SAID

---
+++ Buffs/Debuffs/BlazingSuffering.cs
@@ -0,0 +1,32 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Buffs.Debuffs
+{
+    public class BlazingSuffering : ModBuff
+    {
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("Blazing Suffering");
+            Description.SetDefault("Your body is getting incinerated from the inside.");
+            DisplayName.AddTranslation(GameCulture.German, "Loderndes Leiden");
+            Description.AddTranslation(GameCulture.German, "Dein Körper wird von innen heraus verbrannt");
+            Main.buffNoTimeDisplay[Type] = false;
+            Main.debuff[Type] = true;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.GetModPlayer<MDPlayer>().Blaze = true;
+            player.statDefense -= 5;
+        }
+
+        public override void Update(NPC npc, ref int buffIndex)
+        {
+            npc.GetGlobalNPC<MDGlobalNPC>().Blaze = true;
+            npc.defense -= 5;
+        }
+    }
+}
