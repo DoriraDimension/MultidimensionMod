c7dfaba624ce9374f593d58f72a6359648cfce85
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2021-11-02 23:15:52 +0100

    Ichor still tastes good

---
+++ Projectiles/Boomerangs/InjectorScytheProj.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Projectiles.Boomerangs
+{
+	public class InjectorScytheProj : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Injector");
+		}
+
+		public override void SetDefaults()
+		{
+			projectile.CloneDefaults(ProjectileID.LightDisc);
+			aiType = ProjectileID.LightDisc;
+		}
+
+		public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+		{
+			target.AddBuff(BuffID.Ichor, 180);
+		}
+
+		public override bool PreKill(int timeLeft)
+		{
+			projectile.type = ProjectileID.LightDisc;
+			return true;
+		}
+	}
+}
