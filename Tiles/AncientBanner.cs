640578ab1438c9dd215184ac909333fdb0236637
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2021-11-02 23:22:22 +0100

    Tiles

---
+++ Tiles/AncientBanner.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace MultidimensionMod.Tiles
+{
+	public class AncientBanner : ModTile
+	{
+		public override void SetDefaults()
+		{
+			Main.tileFrameImportant[Type] = true;
+			Main.tileNoAttach[Type] = true;
+			Main.tileLavaDeath[Type] = true;
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style1x2Top);
+			TileObjectData.newTile.Height = 3;
+			TileObjectData.newTile.CoordinateHeights = new[] { 16, 16, 16 };
+			TileObjectData.newTile.StyleHorizontal = true;
+			TileObjectData.newTile.AnchorTop = new AnchorData(AnchorType.SolidTile | AnchorType.SolidSide | AnchorType.SolidBottom, TileObjectData.newTile.Width, 0);
+			TileObjectData.newTile.StyleWrapLimit = 111;
+			TileObjectData.addTile(Type);
+			ModTranslation name = CreateMapEntryName();
+			name.SetDefault("Ancient Banner");
+			AddMapEntry(new Color(183, 154, 104), name);
+		}
+
+		public override void KillMultiTile(int i, int j, int frameX, int frameY)
+		{
+			Item.NewItem(i * 16, j * 16, 64, 32, ModContent.ItemType<Items.Placeables.AncientBannerItem>());
+		}
+	}
+}
---
+++ Tiles/EleanoraPlushiePlaced.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace MultidimensionMod.Tiles
+{
+	public class EleanoraPlushiePlaced : ModTile
+	{
+		public override void SetDefaults()
+		{
+			Main.tileFrameImportant[Type] = true;
+			Main.tileWaterDeath[Type] = true;
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style3x2);
+			TileObjectData.newTile.Height = 4;
+			TileObjectData.newTile.Width = 3;
+			TileObjectData.newTile.CoordinateHeights = new[] { 16, 16, 16, 18 };
+			TileObjectData.addTile(Type);
+			ModTranslation name = CreateMapEntryName();
+			name.SetDefault("Dimensional Goddess Plushie");
+			AddMapEntry(new Color(236, 157, 20), name);
+			dustType = 6;
+			disableSmartCursor = true;
+		}
+
+		public override void NumDust(int i, int j, bool fail, ref int num)
+		{
+			num = 5;
+		}
+
+		public override void KillMultiTile(int i, int j, int frameX, int frameY)
+		{
+			Item.NewItem(i * 16, j * 16, 64, 32, ModContent.ItemType<Items.Placeables.EleanoraPlushiePlaceable>());
+		}
+	}
+}
