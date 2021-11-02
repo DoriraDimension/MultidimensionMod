2302488110f6be3c8ebe606ae2c9f20e1c4b141d
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2021-11-02 22:42:16 +0100

    Ignaen and Smiley JR

---
+++ Items/Pets/ArchtyrantsFace.cs
@@ -0,0 +1,48 @@
+﻿using MultidimensionMod.Buffs.Pets;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Items.Pets
+{
+	public class ArchtyrantsFace : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Archtyrant's Face");
+			Tooltip.SetDefault("Summons the head of Archtyrant Ignaen Moyasu.");
+			DisplayName.AddTranslation(GameCulture.German, "Erztyrann's Gesicht");
+			Tooltip.AddTranslation(GameCulture.German, "Beschwört den Kopf von Erztyrann Ignaen Moyasu.");
+		}
+
+		public override void SetDefaults()
+		{
+			item.CloneDefaults(ItemID.ZephyrFish);
+			item.shoot = ModContent.ProjectileType<Projectiles.Pets.IgnaenHead>();
+			item.buffType = ModContent.BuffType<Buffs.Pets.IgnaenHeadBuff>();
+			item.width = 82;
+			item.height = 62;
+			item.value = Item.sellPrice(silver: 40);
+			item.rare = ItemRarityID.Orange;
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.HellstoneBar, 10);
+			recipe.AddIngredient(ItemID.InfernoPotion);
+			recipe.AddTile(77);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+
+		public override void UseStyle(Player player)
+		{
+			if (player.whoAmI == Main.myPlayer && player.itemTime == 0)
+			{
+				player.AddBuff(item.buffType, 3600, true);
+			}
+		}
+	}
+}
---
+++ Items/Pets/CuteEmoji.cs
@@ -0,0 +1,42 @@
+using MultidimensionMod.Buffs.Pets;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Items.Pets
+{
+	public class CuteEmoji : ModItem
+	{
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Cute Emoji");
+			Tooltip.SetDefault("Summons Smiley's adopted son.");
+			DisplayName.AddTranslation(GameCulture.German, "Niedlicher Emoji");
+			Tooltip.AddTranslation(GameCulture.German, "Beschwört Smiley's adoptierten Sohn.");
+		}
+
+		public override void SetDefaults() {
+			item.CloneDefaults(ItemID.ZephyrFish);
+			item.shoot = ModContent.ProjectileType<Projectiles.Pets.SmileyJr>();
+			item.buffType = ModContent.BuffType<SmileyJrBuff>();
+			item.width = 32;
+			item.height = 32;
+			item.value = Item.sellPrice(silver: 25);
+			item.rare = ItemRarityID.Orange;
+		}
+
+		public override void AddRecipes() {
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ModContent.ItemType<Materials.DarkMatterClump>(), 10);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+
+		public override void UseStyle(Player player) {
+			if (player.whoAmI == Main.myPlayer && player.itemTime == 0) {
+				player.AddBuff(item.buffType, 3600, true);
+			}
+		}
+	}
+}
