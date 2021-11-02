49835ad5be2bb8f00c84f92d04f7e5a49e800193
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2021-11-02 23:18:30 +0100

    Annoying Spears

---
+++ Projectiles/Spears/Tonbogiri2Proj.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Projectiles.Spears
+{
+	public class Tonbogiri2Proj : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Tonbogiri's son");
+		}
+
+		public override void SetDefaults()
+		{
+			projectile.width = 18;
+			projectile.height = 18;
+			projectile.aiStyle = 19;
+			projectile.penetrate = -1;
+			projectile.scale = 1.3f;
+			projectile.alpha = 0;
+
+			projectile.hide = true;
+			projectile.ownerHitCheck = true;
+			projectile.melee = true;
+			projectile.tileCollide = false;
+			projectile.friendly = true;
+		}
+
+		public float movementFactor
+		{
+			get => projectile.ai[0];
+			set => projectile.ai[0] = value;
+		}
+
+		public override void AI()
+		{
+			Player projOwner = Main.player[projectile.owner];
+			Vector2 ownerMountedCenter = projOwner.RotatedRelativePoint(projOwner.MountedCenter, true);
+			projectile.direction = projOwner.direction;
+			projOwner.heldProj = projectile.whoAmI;
+			projOwner.itemTime = projOwner.itemAnimation;
+			projectile.position.X = ownerMountedCenter.X - (float)(projectile.width / 2);
+			projectile.position.Y = ownerMountedCenter.Y - (float)(projectile.height / 2);
+			if (!projOwner.frozen)
+			{
+				if (movementFactor == 0f)
+				{
+					movementFactor = 3f;
+					projectile.netUpdate = true;
+				}
+				if (projOwner.itemAnimation < projOwner.itemAnimationMax / 3)
+				{
+					movementFactor -= 3.3f;
+				}
+				else
+				{
+					movementFactor += 3.3f;
+				}
+			}
+
+			projectile.position += projectile.velocity * movementFactor;
+			if (projOwner.itemAnimation == 0)
+			{
+				projectile.Kill();
+			}
+
+			projectile.rotation = projectile.velocity.ToRotation() + MathHelper.ToRadians(135f);
+			if (projectile.spriteDirection == -1)
+			{
+				projectile.rotation -= MathHelper.ToRadians(90f);
+			}
+		}
+
+		public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+		{
+			target.AddBuff(BuffID.Venom, 900);
+		}
+	}
+}
