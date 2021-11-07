5a48aca2e5d41d3a61ee24e7068c9c2493fd5dff
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2021-11-07 07:52:25 +0100

    Add files via upload

--- Items/Weapons/Ranged/Guns/MetalWormGun.cs
+++ Items/Weapons/Ranged/Guns/MetalWormGun.cs
@@ -69,6 +69,7 @@ namespace MultidimensionMod.Items.Weapons.Ranged.Guns
 		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
 		{
 			type = ModContent.ProjectileType<DestroyerDualLaser>();
+			return Shoot(player, ref position, ref speedX, ref speedY, ref type, ref damage, ref knockBack);
 		}
 	}
 }
