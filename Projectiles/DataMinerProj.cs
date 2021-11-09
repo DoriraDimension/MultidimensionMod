c37ed2a38693b29ee842cd911a473f1f43ce627a
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2021-11-09 22:35:23 +0100

    these actually exist

---
+++ Projectiles/DataMinerProj.cs
@@ -0,0 +1,37 @@
+﻿using MultidimensionMod.Dusts;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Projectiles
+{
+	public class DataMinerProj : ModProjectile
+	{
+		public override void SetDefaults()
+		{
+			projectile.width = 26;
+			projectile.height = 30;
+			projectile.aiStyle = 20;
+			projectile.friendly = true;
+			projectile.penetrate = -1;
+			projectile.tileCollide = false;
+			projectile.hide = true;
+			projectile.ownerHitCheck = true;
+			projectile.melee = true;
+		}
+
+		public override void AI()
+		{
+			int dust = Dust.NewDust(projectile.position, projectile.width, projectile.height, ModContent.DustType<StormDust>(), projectile.velocity.X * 0.2f, projectile.velocity.Y * 0.2f, 100, default(Color), 1.9f);
+			Main.dust[dust].noGravity = true;
+		}
+
+		public override void OnHitNPC(NPC target, int damage, float knockBack, bool crit)
+		{
+			{
+				target.AddBuff(BuffID.Electrified, 60000);
+			}
+		}
+	}
+}
---
+++ Projectiles/FishArrow.cs
@@ -0,0 +1,27 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Projectiles
+{
+	public class FishArrow : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Fish Arrow");
+		}
+
+		public override void SetDefaults()
+		{
+			projectile.arrow = false;
+			projectile.width = 10;
+			projectile.height = 10;
+			projectile.aiStyle = 5;
+			projectile.friendly = true;
+			projectile.ranged = true;
+			projectile.tileCollide = false;
+			projectile.penetrate = 3;
+			aiType = ProjectileID.StarWrath;
+		}
+	}
+}
