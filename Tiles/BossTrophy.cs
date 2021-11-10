fbdeecb0d8a83e1e10144cb6f20cfa8c75ba1577
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2021-11-10 17:54:20 +0100

    This is right

---
+++ Tiles/BossTrophy.cs
@@ -0,0 +1,43 @@
+﻿using MultidimensionMod.Items.Placeables.Trophies;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace MultidimensionMod.Tiles
+{
+	public class BossTrophy : ModTile
+	{
+		public override void SetDefaults()
+		{
+			Main.tileFrameImportant[Type] = true;
+			Main.tileLavaDeath[Type] = true;
+			TileID.Sets.FramesOnKillWall[Type] = true;
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3Wall);
+			TileObjectData.newTile.StyleHorizontal = true;
+			TileObjectData.newTile.StyleWrapLimit = 36;
+			TileObjectData.addTile(Type);
+			dustType = 7;
+			disableSmartCursor = true;
+			ModTranslation name = CreateMapEntryName();
+			name.SetDefault("Trophy");
+			AddMapEntry(new Color(120, 85, 60), name);
+		}
+
+		public override void KillMultiTile(int i, int j, int frameX, int frameY)
+		{
+			int item = 0;
+			switch (frameX / 54)
+			{
+				case 0:
+					item = ModContent.ItemType<SmileyTrophy>();
+					break;
+					if (item > 0)
+					{
+						Item.NewItem(i * 16, j * 16, 48, 48, item);
+					}
+			}
+		}
+	}
+}
