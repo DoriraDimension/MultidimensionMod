1cf6fca0aca92306d5ff083f137479186c68890f
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2021-11-26 11:52:45 +0100

    fixed the balls suddenly not bouncing anymore

--- Projectiles/Magic/SandBallz.cs
+++ Projectiles/Magic/SandBallz.cs
@@ -27,7 +27,7 @@ namespace MultidimensionMod.Projectiles.Magic
 		}
 
 		public override void AI()
-        {
+		{
 			projectile.velocity.Y = projectile.velocity.Y + 0.2f;
 			if (projectile.velocity.Y > 16f)
 			{
@@ -39,6 +39,29 @@ namespace MultidimensionMod.Projectiles.Magic
 			Main.dust[num104].noGravity = true;
 			Main.dust[num104].velocity.X *= 0.3f;
 			Main.dust[num104].velocity.Y *= 0.3f;
+		}
+
+		public override bool OnTileCollide(Vector2 oldVelocity)
+		{
+			projectile.penetrate--;
+			if (projectile.penetrate <= 0)
+			{
+				projectile.Kill();
+			}
+			else
+			{
+				Collision.HitTiles(projectile.position + projectile.velocity, projectile.velocity, projectile.width, projectile.height);
+				Main.PlaySound(SoundID.Item10, projectile.position);
+				if (projectile.velocity.X != oldVelocity.X)
+				{
+					projectile.velocity.X = -oldVelocity.X;
+				}
+				if (projectile.velocity.Y != oldVelocity.Y)
+				{
+					projectile.velocity.Y = -oldVelocity.Y;
+				}
+			}
+			return false;
 		}
 	}
 }
