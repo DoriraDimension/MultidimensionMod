331064145acc0ddc8fc288cec62f0dffbf3247df
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2021-11-30 23:12:41 +0100

    glowmensium

--- Items/Materials/Dimensium.cs
+++ Items/Materials/Dimensium.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.ID;
 using Terraria.Localization;
@@ -33,6 +35,27 @@ namespace MultidimensionMod.Items.Materials
 					item.overrideColor = MDRarity.Dorira;
 				}
 			}
+		}
+
+		public override void PostDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, float rotation, float scale, int whoAmI)
+		{
+			Texture2D texture = mod.GetTexture("Items/Materials/Dimensium_Glow");
+			spriteBatch.Draw
+			(
+				texture,
+				new Vector2
+				(
+					item.position.X - Main.screenPosition.X + item.width * 0.5f,
+					item.position.Y - Main.screenPosition.Y + item.height - texture.Height * 0.5f + 2f
+				),
+				new Rectangle(0, 0, texture.Width, texture.Height),
+				Color.White,
+				rotation,
+				texture.Size() * 0.5f,
+				scale,
+				SpriteEffects.None,
+				0f
+			);
 		}
 	}
 }
