c0612edf69e7514849c4baa1cc5152dac88f6082
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2021-12-02 19:13:43 +0100

    UwU

---
+++ Tiles/CheliaPlushiePlaced.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace MultidimensionMod.Tiles
+{
+	public class CheliaPlushiePlaced : ModTile
+	{
+		public override void SetDefaults()
+		{
+			Main.tileFrameImportant[Type] = true;
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style3x2);
+			TileObjectData.newTile.Height = 4;
+			TileObjectData.newTile.Width = 3;
+			TileObjectData.newTile.CoordinateHeights = new[] { 16, 16, 16, 18 };
+			TileObjectData.addTile(Type);
+			ModTranslation name = CreateMapEntryName();
+			name.SetDefault("Shapeshifter Plushie");
+			AddMapEntry(new Color(225, 71, 173), name);
+			dustType = 228;
+			disableSmartCursor = true;
+		}
+
+		public override void KillMultiTile(int i, int j, int frameX, int frameY)
+		{
+			Item.NewItem(i * 16, j * 16, 64, 32, ModContent.ItemType<Items.Placeables.CheliaPlushiePlaceable>());
+		}
+	}
+}
