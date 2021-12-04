0dd8b1f76cbbb70b63d80ee8f161c0ce03ad98e9
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2021-12-04 05:03:04 +0100

    druggiesss nyaaaaijdhlsd

---
+++ Items/Accessories/BaitLeaf.cs
@@ -0,0 +1,33 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.Utilities;
+
+namespace MultidimensionMod.Items.Accessories
+{
+	public class BaitLeaf : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Bait Leaf");
+			Tooltip.SetDefault("A aromatic leaf, perfect to get in more followers.\nGives +1 minion slot");
+			DisplayName.AddTranslation(GameCulture.German, "Köderblatt");
+			Tooltip.AddTranslation(GameCulture.German, "Ein aromatisches Blatt, perfekt um neue anhänger zu bekommen.\nGibt +1 Minion Slot.");
+		}
+
+		public override void SetDefaults()
+		{
+			item.width = 36;
+			item.height = 30;
+			item.accessory = true;
+			item.value = Item.sellPrice(silver: 3);
+			item.rare = ItemRarityID.Blue;
+		}
+
+		public override void UpdateAccessory(Player player, bool hideVisual)
+		{
+			player.maxMinions += 1;
+		}
+	}
+}
