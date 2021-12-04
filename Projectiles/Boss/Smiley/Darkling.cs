b1e74a8e4286a25986bc7f3fd84e392d8d416cb4
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2021-12-04 03:42:07 +0100

    Now despawns properly

--- Projectiles/Boss/Smiley/Darkling.cs
+++ Projectiles/Boss/Smiley/Darkling.cs
@@ -36,7 +36,7 @@ namespace MultidimensionMod.Projectiles.Boss.Smiley
 
                 Main.PlaySound(SoundID.Item93, projectile.position);
 
-                Dust.NewDust(projectile.position, projectile.width, projectile.height, DustID.Clentaminator_Red, (float)((a.Next() % 100) / 100), (float)((a.Next() % 100) / 100), 0, default, 1.5f);   //spawns dust behind it, this is a spectral light blue dust. 15 is the dust, change that to what you want.
+                Dust.NewDust(projectile.position, projectile.width, projectile.height, 118, (float)((a.Next() % 100) / 100), (float)((a.Next() % 100) / 100), 0, default, 1.5f);   //spawns dust behind it, this is a spectral light blue dust. 15 is the dust, change that to what you want.
 
 
             }
@@ -55,14 +55,13 @@ namespace MultidimensionMod.Projectiles.Boss.Smiley
 
             //this is projectile dust
 
-            int DustID2 = Dust.NewDust(projectile.position + projectile.velocity, projectile.width, projectile.height, DustID.DungeonSpirit, projectile.velocity.X * -0.1f, projectile.velocity.Y * -0.1f, 0, default, 1.25f);   //spawns dust behind it, this is a spectral light blue dust lol
+            int DustID2 = Dust.NewDust(projectile.position + projectile.velocity, projectile.width, projectile.height, 180, projectile.velocity.X * -0.1f, projectile.velocity.Y * -0.1f, 0, default, 1.25f);   //spawns dust behind it, this is a spectral light blue dust lol
             Main.dust[DustID2].noGravity = true;
 
 
             //this make that the projectile faces the right way
             projectile.rotation = (float)Math.Atan2((double)projectile.velocity.Y, (double)projectile.velocity.X) + 1.57f;
 
-            projectile.timeLeft = 999;
             projectile.localAI[0] += 1f;
             //projectile.alpha = (int)projectile.localAI[0] * 2;
 
