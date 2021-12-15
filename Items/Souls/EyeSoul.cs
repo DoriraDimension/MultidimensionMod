96ed787b04a482811fde07a1e480f0cb55396876
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2021-12-15 11:02:24 +0100

    typo in german translation

--- Items/Souls/EyeSoul.cs
+++ Items/Souls/EyeSoul.cs
@@ -15,7 +15,7 @@ namespace MultidimensionMod.Items.Souls
 			DisplayName.SetDefault("Soul of the Eye of Cthulhu");
 			Tooltip.SetDefault("Yes, this eye actually had a soul on its own.\nThe ever watching eye, true eye or not, it doesnt matter, it was just waiting for another being to grow stronger.\nSome people call it the Eye of Judgement, the first test.\nEquip so that Cthulhu's servants wont interrupt your fight.");
 			DisplayName.AddTranslation(GameCulture.German, "Seele des Auges von Cthulhu");
-			Tooltip.AddTranslation(GameCulture.German, "Ja, dieses Auge hatte eine eigene Seele.\nDas immer beobachtende Auge, wahres Auge oder nicht, es ist egal, es hat nur darauf gewartet das ein weiters Wesen an stärke gewinnt.\nEinige Leute nennen es das Auge des Urteils, der erste Test.\nRüste damit Cthulhus Diener deinen Kampf nicht stören.");
+			Tooltip.AddTranslation(GameCulture.German, "Ja, dieses Auge hatte eine eigene Seele.\nDas immer beobachtende Auge, wahres Auge oder nicht, es ist egal, es hat nur darauf gewartet das ein weiters Wesen an stärke gewinnt.\nEinige Leute nennen es das Auge des Urteils, der erste Test.\nRüste aus damit Cthulhus Diener deinen Kampf nicht stören.");
 			Main.RegisterItemAnimation(item.type, new DrawAnimationVertical(7, 12));
 			ItemID.Sets.ItemIconPulse[item.type] = true;
 			ItemID.Sets.ItemNoGravity[item.type] = true;
