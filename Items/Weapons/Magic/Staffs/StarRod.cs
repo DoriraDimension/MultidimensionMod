f923c425b51c8919181099efc68f41afc376bd38
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2021-12-21 19:08:39 +0100

    ugh2

--- Items/Weapons/Magic/Staffs/StarRod.cs
+++ Items/Weapons/Magic/Staffs/StarRod.cs
@@ -26,7 +26,7 @@ namespace MultidimensionMod.Items.Weapons.Magic.Staffs
 			item.damage = 8;
 			item.magic = true;
 			item.noMelee = true;
-			Items.value = Item.sellPrice(copper: 67);
+			item.value = Item.sellPrice(copper: 67);
 			item.mana = 4;
 			item.rare = ItemRarityID.Blue;
 		}
