07e30de7db934a1c5f6d55daca5906a614f1d54a
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2021-12-26 20:45:20 +0100

    Why so much fucking slime

--- NPCs/Tundra/FrostburnSlime.cs
+++ NPCs/Tundra/FrostburnSlime.cs
@@ -68,5 +68,18 @@ namespace MultidimensionMod.NPCs.Tundra
 		{
 			player.AddBuff(BuffID.Frostburn, 360, true);
 		}
+
+		public override void HitEffect(int hitDirection, double damage)
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				int dustType = Main.rand.Next(80, 80);
+				int dustIndex = Dust.NewDust(npc.position, npc.width, npc.height, dustType);
+				Dust dust = Main.dust[dustIndex];
+				dust.velocity.X = dust.velocity.X + Main.rand.Next(-50, 51) * 0.01f;
+				dust.velocity.Y = dust.velocity.Y + Main.rand.Next(-50, 51) * 0.01f;
+				dust.scale *= 1f + Main.rand.Next(-30, 31) * 0.01f;
+			}
+		}
 	}
 }
