417f9575301c3d0ab1533e3e29582a594f79ed08
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2021-12-26 10:16:32 +0100

    More vanity

---
+++ Items/Vanity/CorrGuyMask.cs
@@ -0,0 +1,34 @@
+﻿using MultidimensionMod.Items.Materials;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Items.Vanity
+{
+	[AutoloadEquip(EquipType.Head)]
+	public class CorrGuyMask : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Infested Abomination Mask");
+			Tooltip.SetDefault("Does not include flies.");
+			DisplayName.AddTranslation(GameCulture.German, "Befallene Abscheuligkeit Maske");
+			Tooltip.AddTranslation(GameCulture.German, "Enthält keine Fliegen.");
+		}
+
+		public override void SetDefaults()
+		{
+			item.width = 20;
+			item.height = 20;
+			item.value = Item.sellPrice(silver: 50);
+			item.rare = ItemRarityID.Green;
+			item.vanity = true;
+		}
+
+		public override bool DrawHead()
+		{
+			return false;
+		}
+	}
+}
