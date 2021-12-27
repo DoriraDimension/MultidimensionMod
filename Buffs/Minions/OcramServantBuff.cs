1fc1419491557ed2643aeeaffb7635ead405c930
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2021-12-27 20:07:36 +0100

    Buffbuffy

---
+++ Buffs/Minions/OcramServantBuff.cs
@@ -0,0 +1,36 @@
+﻿using MultidimensionMod.Projectiles.Minions;
+using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Buffs.Minions
+{
+	public class OcramServantBuff : ModBuff
+	{
+		public override void SetDefaults()
+		{
+			DisplayName.SetDefault("Forgotten Servant");
+			Description.SetDefault("The servant of a long lost time is fighting for you");
+			DisplayName.AddTranslation(GameCulture.German, "Vergessener Diener");
+			Description.AddTranslation(GameCulture.German, "Der Diener einer lang vergessenen Zeit kämpft für dich");
+			Main.buffNoSave[Type] = true;
+			Main.buffNoTimeDisplay[Type] = true;
+		}
+
+		public override void Update(Player player, ref int buffIndex)
+		{
+			if (player.ownedProjectileCounts[ModContent.ProjectileType<ServantofOcram2>()] > 0)
+			{
+				player.buffTime[buffIndex] = 18000;
+			}
+			else
+			{
+				player.DelBuff(buffIndex);
+				buffIndex--;
+			}
+		}
+	}
+}
