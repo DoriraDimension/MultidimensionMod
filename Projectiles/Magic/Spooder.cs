e4679e98a9eb2f0819de48a9b4a1fe6946618e32
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-01-05 02:38:28 +0100

    Spooders

---
+++ Projectiles/Magic/Spooder.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Projectiles.Magic
+{
+	internal class Spooder : ModProjectile
+	{
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Spooder");
+		}
+
+		public override void SetDefaults()
+		{
+			projectile.width = 34;
+			projectile.height = 34;
+			projectile.friendly = true;
+			projectile.magic = true;
+			projectile.tileCollide = true;
+		}
+
+		public override void Kill(int timeLeft)
+		{
+			Main.PlaySound(SoundID.Item8, projectile.position);
+
+			for (int i = 0; i < 3; i++)
+			{
+				int dustIndex = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 30, 0f, 0f, 100, default(Color), 2f);
+				Main.dust[dustIndex].velocity *= 1.4f;
+			}
+		}
+
+		public override void AI()
+		{
+			if (projectile.timeLeft > 180)
+				projectile.timeLeft = 180;
+
+			Player player = Main.player[projectile.owner];
+			float distanceFromTarget = 700f;
+			Vector2 targetCenter = projectile.position;
+			bool foundTarget = false;
+
+			if (player.HasMinionAttackTargetNPC)
+			{
+				NPC npc = Main.npc[player.MinionAttackTargetNPC];
+				float between = Vector2.Distance(npc.Center, projectile.Center);
+				if (between < 2000f)
+				{
+					distanceFromTarget = between;
+					targetCenter = npc.Center;
+					foundTarget = true;
+				}
+			}
+			if (!foundTarget)
+			{
+				for (int i = 0; i < Main.maxNPCs; i++)
+				{
+					NPC npc = Main.npc[i];
+					if (npc.CanBeChasedBy())
+					{
+						float between = Vector2.Distance(npc.Center, projectile.Center);
+						bool closest = Vector2.Distance(projectile.Center, targetCenter) > between;
+						bool inRange = between < distanceFromTarget;
+						bool lineOfSight = Collision.CanHitLine(projectile.position, projectile.width, projectile.height, npc.position, npc.width, npc.height);
+						bool closeThroughWall = between < 100f;
+						if (((closest && inRange) || !foundTarget) && (lineOfSight || closeThroughWall))
+						{
+							distanceFromTarget = between;
+							targetCenter = npc.Center;
+							foundTarget = true;
+						}
+					}
+				}
+			}
+
+			projectile.friendly = foundTarget;
+
+			float speed = 8f;
+			float inertia = 20f;
+
+			if (foundTarget)
+			{
+				if (distanceFromTarget > 40f)
+				{
+					Vector2 direction = targetCenter - projectile.Center;
+					direction.Normalize();
+					direction *= speed;
+					projectile.velocity = (projectile.velocity * (inertia - 1) + direction) / inertia;
+				}
+			}
+			else
+			{
+				if (!foundTarget)
+				{
+					speed = 4f;
+					inertia = 60f;
+				}
+				else
+				{
+					speed = 8f;
+					inertia = 300f;
+				}
+
+				if (projectile.spriteDirection == -1)
+				{
+					projectile.rotation += MathHelper.Pi;
+				}
+			}
+
+			projectile.direction = projectile.spriteDirection = projectile.velocity.X > 0f ? 1 : -1;
+			projectile.rotation = projectile.velocity.ToRotation();
+			if (projectile.velocity.Y > 16f)
+			{
+				projectile.velocity.Y = 16f;
+			}
+			if (projectile.spriteDirection == -1)
+			{
+				projectile.rotation += MathHelper.Pi;
+			}
+		}
+	}
+}
---
+++ Projectiles/Magic/Weeeeeeeee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Projectiles.Magic
+{
+	internal class Weeeeeeeee : ModProjectile
+	{
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Sonic the Spooder");
+		}
+
+		public override void SetDefaults()
+		{
+			projectile.width = 38;
+			projectile.height = 38;
+			projectile.friendly = true;
+			projectile.magic = true;
+			projectile.tileCollide = true;
+		}
+
+		public override void Kill(int timeLeft)
+		{
+			Main.PlaySound(SoundID.Item8, projectile.position);
+
+			for (int i = 0; i < 6; i++)
+			{
+				int dustIndex = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 30, 0f, 0f, 100, default(Color), 2f);
+				Main.dust[dustIndex].velocity *= 1.4f;
+			}
+		}
+
+		public override void AI()
+		{
+
+			projectile.direction = projectile.spriteDirection = projectile.velocity.X > 0f ? 1 : -1;
+			projectile.rotation = projectile.velocity.ToRotation();
+			if (projectile.velocity.Y > 16f)
+			{
+				projectile.velocity.Y = 16f;
+			}
+			if (projectile.spriteDirection == -1)
+			{
+				projectile.rotation += MathHelper.Pi;
+			}
+		}
+	}
+}
