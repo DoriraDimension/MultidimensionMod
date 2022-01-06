451e58753ca60e35343d3c445eafe3b73bce2a94
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-01-06 04:59:27 +0100

    Nightvision vision

--- Items/Potions/CookedEyeTendril.cs
+++ Items/Potions/CookedEyeTendril.cs
@@ -1,4 +1,5 @@
 ﻿using MultidimensionMod.Items.Materials;
+using MultidimensionMod.Items.Weapons.Ranged.Bows;
 using MultidimensionMod.Buffs.Potions;
 using Terraria;
 using Terraria.ID;
@@ -30,8 +31,8 @@ namespace MultidimensionMod.Items.Potions
             item.consumable = true;
             item.rare = ItemRarityID.Green;
             item.value = Item.sellPrice(copper: 23);
-            item.buffType = (BuffID.WellFed);
-            item.buffTime = 18000; 
+            item.buffType = (BuffID.NightOwl);
+            item.buffTime = 3600; 
         }
 
         public override void AddRecipes()
@@ -40,6 +41,12 @@ namespace MultidimensionMod.Items.Potions
             recipe.AddIngredient(ModContent.ItemType<Materials.EyeTendril>());
             recipe.AddTile(17);
             recipe.SetResult(this);
+            recipe.AddRecipe();
+
+            recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ModContent.ItemType<TendrilBow>());
+            recipe.AddTile(17);
+            recipe.SetResult(this, 12);
             recipe.AddRecipe();
 
         }
