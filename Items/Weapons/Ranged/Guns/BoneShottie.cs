ae3f122ff90ec5de6430b440b6e81d483a2cdafe
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-01-25 20:56:14 +0100

    Sans WW2

---
+++ Items/Weapons/Ranged/Guns/BoneShottie.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using MultidimensionMod.Items.Materials;
+using Terraria;
+using Microsoft.Xna.Framework;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Items.Weapons.Ranged.Guns
+{
+	public class BoneShottie : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Bone Shottie");
+			Tooltip.SetDefault("Spooky Scare Shotgun.");
+			DisplayName.AddTranslation(GameCulture.German, "Knochen Schrotze");
+			Tooltip.AddTranslation(GameCulture.German, "Spooky Scary Shotgun.");
+		}
+
+		public override void SetDefaults()
+		{
+			item.damage = 32;
+			item.ranged = true;
+			item.width = 64;
+			item.height = 22;
+			item.useTime = 52;
+			item.useAnimation = 52;
+			item.useStyle = ItemUseStyleID.HoldingOut;
+			item.noMelee = true;
+			item.knockBack = 4;
+			item.value = Item.sellPrice(silver: 10);
+			item.rare = ItemRarityID.Orange;
+			item.UseSound = SoundID.Item38;
+			item.autoReuse = true;
+			item.shoot = 10;
+			item.shootSpeed = 11f;
+			item.useAmmo = AmmoID.Bullet;
+		}
+
+		public override Vector2? HoldoutOffset()
+		{
+			return new Vector2(-8, 0);
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.Bone, 34);
+			recipe.AddTile(300);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+
+		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+		{
+			int numberProjectiles = 4;
+			for (int i = 0; i < numberProjectiles; i++)
+			{
+				Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedByRandom(MathHelper.ToRadians(18));																												
+				Projectile.NewProjectile(position.X, position.Y, perturbedSpeed.X, perturbedSpeed.Y, type, damage, knockBack, player.whoAmI);
+			}
+			return false;
+		}
+	}
+}
