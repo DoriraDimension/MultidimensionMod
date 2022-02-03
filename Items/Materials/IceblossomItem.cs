3e82281dd3363cdee6390bff252700fdc97846bf
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-02-03 15:31:35 +0100

    disneys frozen

--- Items/Materials/IceblossomItem.cs
+++ Items/Materials/IceblossomItem.cs
@@ -11,22 +11,24 @@ namespace MultidimensionMod.Items.Materials
 		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Iceblossom");
-			Tooltip.SetDefault("A kind of flora found in the Frozen Underworld, its ancestor, the Fireblossom, adapted to the environment to survive.");
+			Tooltip.SetDefault("A kind of flora found in the Frozen Underworld, its ancestor, the Fireblossom, adapted to the environment to survive.\nThe influence of the icy magic made this plant dangerous to eat.");
 			DisplayName.AddTranslation(GameCulture.German, "Eisblüte");
-			Tooltip.AddTranslation(GameCulture.German, "Eine Art von Flora die in der Gefrorenen Unterwelt gefunden wird, ihr vorfahre, die Feuerblüte, hat sich an die neue Umgebung angepasst um zu überleben.");
+			Tooltip.AddTranslation(GameCulture.German, "Eine Art von Flora die in der Gefrorenen Unterwelt gefunden wird, ihr vorfahre, die Feuerblüte, hat sich an die neue Umgebung angepasst um zu überleben.\nDie auswirkungen der eisigen Magie haben die Pflanze gefährlich gemacht zu essen.");
 		}
 
 		public override void SetDefaults()
 		{
-			item.autoReuse = true;
-			item.useTurn = true;
-			item.useStyle = ItemUseStyleID.SwingThrow;
-			item.useAnimation = 15;
-			item.useTime = 10;
 			item.maxStack = 999;
 			item.width = 12;
 			item.height = 14;
 			item.value = 80;
+			item.useStyle = ItemUseStyleID.EatingUsing;
+			item.useAnimation = 15;
+			item.useTime = 15;
+			item.UseSound = SoundID.Item2;
+			item.consumable = true;
+			item.buffType = (47); //Frozen
+			item.buffTime = 900;
 		}
 	}
 }
