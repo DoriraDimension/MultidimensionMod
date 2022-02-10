5bf709a0a1d0516f9a7e6907ecb6aa54da0e7893
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-02-10 03:36:10 +0100

    moondust mmmm

---
+++ Dusts/Moondust.cs
@@ -0,0 +1,31 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Dusts
+{
+	public class Moondust : ModDust
+	{
+		public override void OnSpawn(Dust dust)
+		{
+			dust.noGravity = true;
+			dust.scale = 1f;
+		}
+
+		public override bool Update(Dust dust)
+		{
+			dust.position += dust.velocity;
+			dust.rotation += dust.velocity.X;
+			dust.scale -= 0.1f;
+			if (dust.scale < 2.5f)
+			{
+				dust.active = false;
+			}
+			else
+			{
+				float strength = dust.scale / 2f;
+				Lighting.AddLight((int)(dust.position.X / 16f), (int)(dust.position.Y / 16f), dust.color.R / 255f * 0.5f * strength, dust.color.G / 255f * 0.5f * strength, dust.color.B / 255f * 0.5f * strength);
+			}
+			return false;
+		}
+	}
+}
