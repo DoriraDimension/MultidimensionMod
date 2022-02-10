ad5d0b9794e9787b15a4164e791b82a09ece0ff5
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-02-10 03:35:46 +0100

    MOOOOOOOOOOOOOOOOOOOOOOOON

---
+++ Items/Weapons/Magic/Other/GreatMoonlight.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using MultidimensionMod.Items.Materials;
+using MultidimensionMod.Items.Souls;
+using MultidimensionMod.Projectiles.Magic;
+using MultidimensionMod.Dusts;
+using MultidimensionMod.Tiles;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Items.Weapons.Magic.Other
+{
+	public class GreatMoonlight : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Great Moonlight");
+			Tooltip.SetDefault("The blue crystal blade is composed only of light.\nThe lunatic Cultist is a insane fanatic of the moon and so stole this legendary blade to use it himself,\ntho the Great Moonlight seems to not belong in this worldand instead obeys an otherworldly moon.");
+			DisplayName.AddTranslation(GameCulture.German, "Großes Mondlicht");
+			Tooltip.AddTranslation(GameCulture.German, "Die blaue Kristallklinge ist nur aus Licht zusammengesetzt.\nDer Irre Kultist ist ein Fanatier des Mondes und so stahl er diese Legendäre Klinge um sie selbst zu benutzen,\nallerdings scheint es als gehöre das Große Mondlicht nicht in diese Welt und stattdessen gehorcht einem jenseitigen Mond.");
+		}
+
+		public override void SetDefaults()
+		{
+			item.damage = 100;
+			item.magic = true;
+			item.width = 72;
+			item.height = 72;
+			item.useTime = 61;
+			item.useAnimation = 30;
+			item.useStyle = 1;
+			item.knockBack = 4;
+			item.autoReuse = true;
+			item.value = Item.sellPrice(gold: 1);
+			item.rare = ItemRarityID.Pink;
+			item.UseSound = SoundID.Item1;
+			item.shoot = ModContent.ProjectileType<MoonlightWave>();
+			item.shootSpeed = 15f;
+		}
+
+		public override void MeleeEffects(Player player, Rectangle hitbox)
+		{
+			for (int i = 0; i < 6; i++)
+			{
+				int dustIndex = Dust.NewDust(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, (ModContent.DustType<Moondust>()));
+				Main.dust[dustIndex].noGravity = true;
+			}
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ModContent.ItemType<CultistSoul>());
+			recipe.AddIngredient(ModContent.ItemType<Dimensium>(), 20);
+			recipe.AddTile(ModContent.TileType<DimensionalForge>());
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
