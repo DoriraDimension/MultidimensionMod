af742da5e21d525d896061cdc2aa05d65796f74d
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-04-01 00:06:54 +0200

    Cold music lol rofl xy

---
+++ Tiles/MusicBoxes/ColdHellBoxPlaced.cs
@@ -0,0 +1,40 @@
+﻿using MultidimensionMod.Items.Placeables.MusicBoxes;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace MultidimensionMod.Tiles.MusicBoxes
+{
+	internal class ColdHellBoxPlaced : ModTile
+	{
+		public override void SetDefaults()
+		{
+			Main.tileFrameImportant[Type] = true;
+			Main.tileObsidianKill[Type] = true;
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
+			TileObjectData.newTile.Origin = new Point16(0, 1);
+			TileObjectData.newTile.LavaDeath = false;
+			TileObjectData.newTile.DrawYOffset = 2;
+			TileObjectData.addTile(Type);
+			disableSmartCursor = true;
+			ModTranslation name = CreateMapEntryName();
+			name.SetDefault("Music Box");
+			AddMapEntry(new Color(200, 200, 200), name);
+		}
+
+		public override void KillMultiTile(int i, int j, int frameX, int frameY)
+		{
+			Item.NewItem(i * 16, j * 16, 16, 48, ModContent.ItemType<ColdHellBox>());
+		}
+
+		public override void MouseOver(int i, int j)
+		{
+			Player player = Main.LocalPlayer;
+			player.noThrow = 2;
+			player.showItemIcon = true;
+			player.showItemIcon2 = ModContent.ItemType<ColdHellBox>();
+		}
+	}
+}
