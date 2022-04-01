5fcdf8921b2b5bc649be7892a805a03ba2922437
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-04-01 05:10:18 +0200

    FINALLY

--- Items/Weapons/Magic/Staffs/StarRod.cs
+++ Items/Weapons/Magic/Staffs/StarRod.cs
@@ -1,4 +1,5 @@
 ﻿using MultidimensionMod.Projectiles.Magic;
+using System;
 using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
@@ -19,22 +20,52 @@ namespace MultidimensionMod.Items.Weapons.Magic.Staffs
 
 		public override void SetDefaults()
 		{
-			item.CloneDefaults(ItemID.Starfury);
-			item.shootSpeed *= 0.65f;
+			item.damage = 8;
 			item.width = 26;
 			item.height = 26;
-			item.damage = 8;
+			item.useStyle = 1;
+			item.useTime = 20;
+			item.useAnimation = 20;
+			item.knockBack = 2f;
 			item.magic = true;
 			item.noMelee = true;
 			item.value = Item.sellPrice(copper: 67);
-			item.mana = 4;
 			item.rare = ItemRarityID.Blue;
+			item.mana = 4;
+			item.shoot = ModContent.ProjectileType<PoyoStar>();
+			item.shootSpeed = 10f;
 		}
 
 		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
 		{
-			type = ModContent.ProjectileType<PoyoStar>();
-			return base.Shoot(player, ref position, ref speedX, ref speedY, ref type, ref damage, ref knockBack);
+			int Damage = item.damage;
+			float KnockBack = item.knockBack;
+			int shoot = item.shoot;
+			float speed = item.shootSpeed;
+			float num70 = (float)Main.mouseX + Main.screenPosition.X - position.X;
+			float num71 = (float)Main.mouseY + Main.screenPosition.Y - position.Y;
+			float num72 = (float)Math.Sqrt(num70 * num70 + num71 * num71);
+			int num99 = 2;
+				position = new Vector2(player.position.X + (float)player.width * 0.5f + (float)(Main.rand.Next(201) * -player.direction) + ((float)Main.mouseX + Main.screenPosition.X - player.position.X), player.MountedCenter.Y - 600f);
+				position.X = (position.X + player.Center.X) / 2f + (float)Main.rand.Next(-200, 201);
+				num70 = (float)Main.mouseX + Main.screenPosition.X - position.X;
+				num71 = (float)Main.mouseY + Main.screenPosition.Y - position.Y;
+				if (num71 < 0f)
+				{
+					num71 *= -1f;
+				}
+				if (num71 < 20f)
+				{
+					num71 = 20f;
+				}
+				num72 = (float)Math.Sqrt(num70 * num70 + num71 * num71);
+				num72 = speed / num72;
+				num70 *= num72;
+				num71 *= num72;
+				float speedX4 = num70 + (float)Main.rand.Next(-40, 41) * 0.02f;
+				float speedY5 = num71 + (float)Main.rand.Next(-40, 41) * 0.02f;
+				Projectile.NewProjectile(position.X, position.Y, speedX4, speedY5, shoot, Damage, KnockBack);
+			return false;
 		}
 
 		public override void AddRecipes()
