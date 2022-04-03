99ec4b1c335d65763ce073f4922f40497f7ff1b5
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-04-03 07:49:48 +0200

    too strong

--- Items/Weapons/Melee/Boomerangs/FleshRipper.cs
+++ Items/Weapons/Melee/Boomerangs/FleshRipper.cs
@@ -25,7 +25,7 @@ namespace MultidimensionMod.Items.Weapons.Melee.Boomerangs
 			item.shootSpeed *= 2f;
 			item.width = 30;
 			item.height = 42;
-			item.damage = 29;
+			item.damage = 19;
 			item.autoReuse = true;
 			item.value = Item.sellPrice(silver: 25);
 			item.rare = ItemRarityID.Green;
