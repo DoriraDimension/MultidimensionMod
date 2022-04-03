e077d01e87b5dc4206d6a1d730a26c8a4a750527
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-04-03 03:52:01 +0200

    fire is hot like the-

--- Items/Weapons/Melee/Swords/EleanorasBat.cs
+++ Items/Weapons/Melee/Swords/EleanorasBat.cs
@@ -12,9 +12,9 @@ namespace MultidimensionMod.Items.Weapons.Melee.Swords
 		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Dimensional Goddess' Bat");
-			Tooltip.SetDefault("Enemies will Bleed and burn on hit.\nDa Bat.");
+			Tooltip.SetDefault("Enemies will Bleed and burn on hit.\nRight click to shoot multiple fireballs.\nDa Bat.");
 			DisplayName.AddTranslation(GameCulture.German, "Baseballschläger der Dimensions Göttin");
-			Tooltip.AddTranslation(GameCulture.German, "Gegner werden bluten und brennen wenn getroffen.\nDa Bat");
+			Tooltip.AddTranslation(GameCulture.German, "Gegner werden bluten und brennen wenn getroffen.\nRight click to shoot multiple fireballs.\nDa Bat");
 		}
 
 		public override void SetDefaults()
@@ -46,10 +46,44 @@ namespace MultidimensionMod.Items.Weapons.Melee.Swords
 			}
 		}
 
+		public override bool AltFunctionUse(Player player)
+		{
+			return true;
+		}
+
 		public override bool CanUseItem(Player player)
 		{
-			return  NPC.downedMoonlord || (player.name == "Eleanora") || (player.name == "EleanoraKitti") || (player.name == "Ellie") || (player.name == "Sunclaw");
+			if (player.altFunctionUse == 2)
+			{
+				item.damage = 359;
+				item.melee = true;
+				item.useStyle = ItemUseStyleID.SwingThrow;
+				item.useTime = 35;
+				item.useAnimation = 35;
+				item.reuseDelay = 24;
+				item.knockBack = 6;
+				item.UseSound = SoundID.Item1;
+				item.autoReuse = true;
+				item.shoot = ProjectileID.ApprenticeStaffT3Shot;
+				item.shootSpeed = 13f;
+				item.crit = 69;
+			}
+			else
+			{
+				item.damage = 696;
+				item.melee = true;
+				item.useStyle = ItemUseStyleID.SwingThrow;
+				item.useTime = 15;
+				item.useAnimation = 15;
+				item.knockBack = 15;
+				item.UseSound = SoundID.Item1;
+				item.autoReuse = true;
+				item.crit = 69;
+				item.shoot = ProjectileID.None;
+			}
+			return base.CanUseItem(player);
 		}
+
 
 		public override void MeleeEffects(Player player, Rectangle hitbox)
 		{
@@ -63,6 +97,20 @@ namespace MultidimensionMod.Items.Weapons.Melee.Swords
 		{
 			target.AddBuff(BuffID.OnFire, 600);
 			target.AddBuff(BuffID.Bleeding, 600);
+		}
+
+		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+		{
+			if (player.altFunctionUse == 2)
+			{
+				int numberProjectiles = 4 + Main.rand.Next(1);
+				for (int i = 0; i < numberProjectiles; i++)
+				{
+					Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedByRandom(MathHelper.ToRadians(18));
+					Projectile.NewProjectile(position.X, position.Y, perturbedSpeed.X, perturbedSpeed.Y, type, damage, knockBack, player.whoAmI);
+				}
+			}
+			return false;
 		}
 
 		public override void AddRecipes()
