745014d333060aa240f608b00c9d22b5d3ee8927
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-04-23 12:39:11 +0200

    oh yeah projectile folder is bacc

---
+++ Projectiles/Ammo/ForbiddenArrow.cs
@@ -0,0 +1,44 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.Audio;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Projectiles.Ammo
+{
+	public class ForbiddenArrow : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Forbidden Arrow");
+			ProjectileID.Sets.TrailCacheLength[Projectile.type] = 5;
+			ProjectileID.Sets.TrailingMode[Projectile.type] = 0;
+		}
+
+		public override void SetDefaults()
+		{
+			Projectile.width = 18;
+			Projectile.height = 18;
+			Projectile.aiStyle = 1;
+			Projectile.friendly = true;
+			Projectile.hostile = false;
+			Projectile.DamageType = DamageClass.Ranged;
+			Projectile.timeLeft = 600;
+			Projectile.light = 0.2f;
+			Projectile.ignoreWater = false;
+			Projectile.tileCollide = true;
+			Projectile.extraUpdates = 1;
+			AIType = 1;
+		}
+
+		public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+		{
+			target.AddBuff(BuffID.Slow, 300);
+		}
+
+		public override void Kill(int timeLeft)
+		{
+			Collision.HitTiles(Projectile.position + Projectile.velocity, Projectile.velocity, Projectile.width, Projectile.height);
+			SoundEngine.PlaySound(SoundID.Item10, Projectile.position);
+		}
+	}
+}
