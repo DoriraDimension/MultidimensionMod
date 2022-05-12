0d6e6513b7311e4f68812aeefc00a4b83b40cc79
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-05-12 23:45:44 +0200

    rarity renames

--- MDRarity.cs
+++ MDRarity.cs
@@ -14,12 +14,12 @@ namespace MultidimensionMod
 	{
 		public static Color RelicWeapon => new Color(125, 65, 47);
 
-		public static Color RareVariant => new Color(95, 57, 249);
+		public static Color ParallelVariant => new Color(95, 57, 249);
 
 		public static Color Eleanora => new Color(247, 171, 18);
 
 		public static Color Dorira => new Color(18, 279, 247);
 
-		public static Color BossWeapon => new Color(130, 0, 0);
+		public static Color BossItem => new Color(130, 0, 0);
 	}
 }
