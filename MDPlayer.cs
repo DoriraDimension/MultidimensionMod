678b313e7c7110cefc6797055884044733bb8fe0
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-05-15 22:22:33 +0200

    fish

--- MDPlayer.cs
+++ MDPlayer.cs
@@ -5,5 +5,15 @@ namespace MultidimensionMod
     public class MDPlayer : ModPlayer
     {
         public bool Healthy;
+
+        public bool SmileyJr = false;
+
+        public bool IgnaenHead = false;
+
+        public override void ResetEffects()
+        {
+            SmileyJr = false;
+            IgnaenHead = false;
+        }
     }
 }
