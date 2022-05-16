285b81909a92b578df1e26e9113c67db135a1e90
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-05-17 00:49:32 +0200

    omg chicken nuggets

---
+++ MDConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.Runtime.Serialization;
+using Terraria;
+using Terraria.ModLoader.Config;
+
+namespace MultidimensionMod
+{
+    public class MDConfig : ModConfig
+    {
+        public static MDConfig Instance;
+
+        public override ConfigScope Mode => ConfigScope.ClientSide;
+
+        [Label("Town NPCs Sell New Items")]
+        [DefaultValue(true)]
+        [Tooltip("Adds new items to Town NPCs shops.")]
+
+        public bool DisableNPCItemSelling { get; set; }
+
+
+    }
+}
