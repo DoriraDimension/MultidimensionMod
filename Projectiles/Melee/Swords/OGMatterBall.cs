a69d04ad530d7577bebc4552549aceb17836a834
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-05-17 01:22:23 +0200

    Dark Matter's Balls

---
+++ Projectiles/Melee/Swords/OGMatterBall.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.Audio;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Projectiles.Melee.Swords
+{
+	internal class OGMatterBall : ModProjectile
+	{
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Dark Matter Ball");
+			Main.projFrames[Projectile.type] = 3;
+		}
+
+		public override void SetDefaults()
+		{
+			Projectile.width = 28;
+			Projectile.height = 28;
+			Projectile.friendly = true;
+			Projectile.DamageType = DamageClass.Melee;
+			Projectile.ignoreWater = true;
+			Projectile.tileCollide = true;
+			Projectile.hide = false;
+			Projectile.penetrate = 2;
+		}
+
+		public override void AI()
+        {
+			if (Projectile.timeLeft > 270)
+				Projectile.timeLeft = 270;
+
+			if (++Projectile.frameCounter >= 5)
+			{
+				Projectile.frameCounter = 0;
+				if (++Projectile.frame >= 4)
+				{
+					Projectile.frame = 0;
+				}
+			}
+		}
+
+		public override bool OnTileCollide(Vector2 oldVelocity)
+		{
+			Projectile.penetrate--;
+			if (Projectile.penetrate <= 0)
+			{
+				Projectile.Kill();
+			}
+			else
+			{
+				Projectile.ai[0] += 0.1f;
+				if (Projectile.velocity.X != oldVelocity.X)
+				{
+					Projectile.velocity.X = -oldVelocity.X;
+				}
+				if (Projectile.velocity.Y != oldVelocity.Y)
+				{
+					Projectile.velocity.Y = -oldVelocity.Y;
+				}
+				SoundEngine.PlaySound(SoundID.Item10, Projectile.position);
+			}
+			return false;
+		}
+	}
+}
