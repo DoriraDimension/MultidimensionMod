90b5d69c81e349d867f5619088c5b68395733c7b
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-05-16 23:34:06 +0200

    Euronen

---
+++ MDCurrency.cs
@@ -0,0 +1,32 @@
+﻿using MultidimensionMod.Items.Materials;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.GameContent.UI;
+using Terraria.Localization;
+
+namespace MultidimensionMod
+{
+	public class MDCurrency : CustomCurrencySingleCoin
+	{
+		public Color DimensiumTextColor = Color.Cyan;
+
+		public MDCurrency(int coinItemID, long currencyCap) : base(coinItemID, currencyCap)
+		{
+		}
+
+
+		public override void GetPriceText(string[] lines, ref int currentLine, int price)
+		{
+			Color color = DimensiumTextColor * ((float)Main.mouseTextColor / 255f);
+			lines[currentLine++] = string.Format("[c/{0:X2}{1:X2}{2:X2}:{3} {4} {5}]", new object[]
+				{
+					color.R,
+					color.G,
+					color.B,
+					Language.GetTextValue("LegacyTooltip.50"),
+					price,
+					"Dimensium"
+				});
+		}
+	}
+}
