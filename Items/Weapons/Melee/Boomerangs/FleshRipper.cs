cc6df8ad19c83073d9d3588055f95497e36a4707
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-05-30 22:39:20 +0200

    a boomerang is

---
+++ Items/Weapons/Melee/Boomerangs/FleshRipper.cs
@@ -0,0 +1,41 @@
+﻿using MultidimensionMod.Projectiles.Melee.Boomerangs;
+using MultidimensionMod.Items.Materials;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Items.Weapons.Melee.Boomerangs
+{
+	public class FleshRipper : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Flesh Ripper");
+			Tooltip.SetDefault("A throwable Scythe that was restored from old relics, perfect for making big wounds.");
+		}
+
+		public override void SetDefaults()
+		{
+			Item.CloneDefaults(ItemID.WoodenBoomerang);
+			Item.shootSpeed *= 2f;
+			Item.width = 30;
+			Item.height = 42;
+			Item.damage = 19;
+			Item.autoReuse = true;
+			Item.value = Item.sellPrice(silver: 25);
+			Item.rare = ItemRarityID.Green;
+			Item.shoot = ModContent.ProjectileType<FleshRipperProj>();
+		}
+
+		public override void AddRecipes()
+		{
+			CreateRecipe()
+			.AddIngredient(ModContent.ItemType<OldStainedScythe>())
+			.AddIngredient(ItemID.TissueSample, 20)
+			.AddTile(377)
+			.Register();
+		}
+	}
+}
---
+++ Items/Weapons/Melee/Boomerangs/InjectorScythe.cs
@@ -0,0 +1,41 @@
+﻿using MultidimensionMod.Projectiles.Melee.Boomerangs;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Items.Weapons.Melee.Boomerangs
+{
+	public class InjectorScythe : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Injector Scythe");
+			Tooltip.SetDefault("A throwable scythe that was restored from old relics, inflicts the ichor debuff on hit for 3 seconds.");
+		}
+
+		public override void SetDefaults()
+		{
+			Item.CloneDefaults(ItemID.Bananarang);
+			Item.shootSpeed *= 1f;
+			Item.width = 38;
+			Item.height = 52;
+			Item.damage = 65;
+			Item.useTime = 25;
+			Item.useAnimation = 25;
+			Item.value = Item.sellPrice(silver: 78);
+			Item.rare = ItemRarityID.LightRed;
+			Item.shoot = ModContent.ProjectileType<InjectorScytheProj>();
+		}
+
+		public override void AddRecipes()
+		{
+			CreateRecipe()
+			.AddIngredient(ModContent.ItemType<FleshRipper>())
+			.AddIngredient(ItemID.Ichor, 8)
+			.AddTile(134)
+			.Register();
+		}
+	}
+}
