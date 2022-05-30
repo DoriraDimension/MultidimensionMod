8a05ae1a69ed5b28b806a0c997b72dcb8b928769
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-05-30 21:01:39 +0200

    why didnt I commit this shit?

---
+++ Projectiles/Melee/Others/DataMinerProj.cs
@@ -0,0 +1,37 @@
+﻿using MultidimensionMod.Dusts;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Projectiles.Melee.Others
+{
+	public class DataMinerProj : ModProjectile
+	{
+		public override void SetDefaults()
+		{
+			Projectile.width = 26;
+			Projectile.height = 30;
+			Projectile.aiStyle = 20;
+			Projectile.friendly = true;
+			Projectile.penetrate = -1;
+			Projectile.tileCollide = false;
+			Projectile.hide = true;
+			Projectile.ownerHitCheck = true;
+			Projectile.DamageType = DamageClass.Melee;
+		}
+
+		public override void AI()
+		{
+			int dust = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, ModContent.DustType<StormDust>(), Projectile.velocity.X * 0.2f, Projectile.velocity.Y * 0.2f, 100, default(Color), 1.9f);
+			Main.dust[dust].noGravity = true;
+		}
+
+		public override void OnHitNPC(NPC target, int damage, float knockBack, bool crit)
+		{
+			{
+				target.AddBuff(BuffID.Electrified, 60000);
+			}
+		}
+	}
+}
