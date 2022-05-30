91099bd09b3a522788ccf89ce3f8fdf1b9d1d180
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-05-31 00:43:55 +0200

    blazen

---
+++ Buffs/Debuffs/BlazingSuffering.cs
@@ -0,0 +1,30 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Buffs.Debuffs
+{
+    public class BlazingSuffering : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Blazing Suffering");
+            Description.SetDefault("Your body is getting incinerated from the inside.");
+           Main.buffNoTimeDisplay[Type] = false;
+            Main.debuff[Type] = true;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.GetModPlayer<MDPlayer>().Blaze = true;
+            player.statDefense -= 5;
+        }
+
+        public override void Update(NPC npc, ref int buffIndex)
+        {
+            npc.GetGlobalNPC<MDGlobalNPC>().Blaze = true;
+            npc.defense -= 5;
+        }
+    }
+}
