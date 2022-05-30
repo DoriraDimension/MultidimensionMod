3a3f829a02a60e1c776065211de5595a9b38dde7
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-05-30 20:04:19 +0200

    sorry mom, it wasnt my fault

---
+++ Items/Placeables/EleanoraBodypillowItem.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Items.Placeables
+{
+	public class EleanoraBodypillowItem : ModItem
+	{
+		public override void SetStaticDefaults()
+        {
+			DisplayName.SetDefault("Dimensional Goddess Bodypillow");
+			Tooltip.SetDefault("A Dimensional Goddess Bodypillow for your bedroom.");
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 30;
+			Item.height = 48;
+			Item.maxStack = 9999;
+			Item.useTurn = true;
+			Item.autoReuse = true;
+			Item.useAnimation = 15;
+			Item.useTime = 10;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.consumable = true;
+			Item.rare = ItemRarityID.Red;
+			Item.value = Item.sellPrice(silver: 69);
+			Item.createTile = ModContent.TileType<Tiles.EleanoraBodypillow>();
+		}
+
+		public override void ModifyTooltips(List<TooltipLine> list)
+		{
+			foreach (TooltipLine Item in list)
+			{
+				if (Item.Mod == "Terraria" && Item.Name == "ItemName")
+				{
+					Item.OverrideColor = MDRarity.Eleanora;
+				}
+			}
+		}
+
+		public override void AddRecipes()
+		{
+			CreateRecipe()
+			.AddIngredient(ItemID.Silk, 69)
+			.AddIngredient(ItemID.LivingFireBlock, 69)
+			.AddTile(TileID.Loom)
+			.Register();
+		}
+	}
+}
