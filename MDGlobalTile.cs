3188a586675a14000917831efb547c74d6c3fa5b
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-05-31 14:10:37 +0200

    growing bushes

---
+++ MDGlobalTile.cs
@@ -0,0 +1,18 @@
+﻿using MultidimensionMod.Tiles.FrozenUnderworld; 
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod
+{
+    public class MDGlobalTile : GlobalTile
+    {
+        public override void RandomUpdate(int i, int j, int type)
+        {
+            if (type == ModContent.TileType<ColdAsh>() && Main.rand.Next(1000) == 0)
+            {
+                WorldGen.PlaceTile(i, j - 1, ModContent.TileType<Iceblossom>(), mute: true);
+            }
+        }
+    }
+}
