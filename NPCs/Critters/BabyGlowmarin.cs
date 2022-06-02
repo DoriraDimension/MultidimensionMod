4d4fcf8715600c475eb736b38c5cb6b9c47b95b7
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-06-02 03:04:18 +0200

    Fosh

---
+++ NPCs/Critters/BabyGlowmarin.cs
@@ -0,0 +1,75 @@
+﻿using MultidimensionMod.Items.Placeables.Banners;
+using MultidimensionMod.Items.Critters;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader.Utilities;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.NPCs.Critters
+{
+	internal class BabyGlowmarin : ModNPC
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Baby Glowmarin");
+			Main.npcFrameCount[NPC.type] = Main.npcFrameCount[NPCID.Shark];
+			Main.npcCatchable[NPC.type] = true;
+		}
+
+		public override void SetDefaults()
+		{
+			NPC.width = 34;
+			NPC.height = 22;
+			NPC.aiStyle = 16;
+			NPC.damage = 0;
+			NPC.defense = 0;
+			NPC.lifeMax = 5;
+			NPC.HitSound = SoundID.NPCHit51;
+			NPC.DeathSound = SoundID.NPCDeath1;
+			NPC.CloneDefaults(NPCID.Goldfish);
+			NPC.catchItem = (short)ModContent.ItemType<BabyGlowmarinItem>();
+			NPC.friendly = true; 
+			AIType = NPCID.Goldfish;
+			AnimationType = NPCID.Shark;
+			Banner = NPC.type;
+			BannerItem = ModContent.ItemType<BabyGlowmarinBanner>();
+		}
+
+		public override bool? CanBeHitByItem(Player player, Item item)
+		{
+			return true;
+		}
+
+		public override bool? CanBeHitByProjectile(Projectile projectile)
+		{
+			return true;
+		}
+
+		public override float SpawnChance(NPCSpawnInfo spawnInfo)
+		{
+			return !spawnInfo.PlayerSafe && Main.hardMode && NPC.downedMechBossAny ? SpawnCondition.OceanMonster.Chance * 0.30f : 0f;
+		}
+
+		public override void HitEffect(int hitDirection, double damage)
+		{
+			if (NPC.life <= 0)
+			{
+				for (int i = 0; i < 6; i++)
+				{
+					int dust = Dust.NewDust(NPC.position, NPC.width, NPC.height, 200, 2 * hitDirection, -2f);
+					if (Main.rand.NextBool(2))
+					{
+						Main.dust[dust].noGravity = true;
+						Main.dust[dust].scale = 1.2f * NPC.scale;
+					}
+					else
+					{
+						Main.dust[dust].scale = 0.7f * NPC.scale;
+					}
+				}
+				Gore.NewGore(NPC.GetSource_FromThis(), NPC.position, NPC.velocity, ModContent.Find<ModGore>("MultidimensionMod/BabyMarinGore1").Type, 1);
+				Gore.NewGore(NPC.GetSource_FromThis(), NPC.position, NPC.velocity, ModContent.Find<ModGore>("MultidimensionMod/BabyMarinGore2").Type, 1);
+			}
+		}
+	}
+}
