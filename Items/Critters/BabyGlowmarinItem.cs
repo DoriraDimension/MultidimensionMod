6c50409d42463e2acc2aeca26216d36ad5424d80
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-06-02 03:04:41 +0200

    Fesh

---
+++ Items/Critters/BabyGlowmarinItem.cs
@@ -0,0 +1,34 @@
+﻿using MultidimensionMod.NPCs.Critters;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Items.Critters
+{
+	internal class BabyGlowmarinItem : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Baby Glowmarin");
+			Tooltip.SetDefault("A juvenile Glowmarin, it's glow organs are already developed.");
+		}
+
+		public override void SetDefaults()
+		{
+			Item.useStyle = 1;
+			Item.autoReuse = true;
+			Item.useTurn = true;
+			Item.useAnimation = 15;
+			Item.useTime = 10;
+			Item.maxStack = 999;
+			Item.consumable = true;
+			Item.width = 34;
+			Item.height = 22;
+			Item.makeNPC = 360;
+			Item.noUseGraphic = true;
+			Item.CloneDefaults(ItemID.GlowingSnail);
+			Item.bait = 45;
+			Item.makeNPC = (short)ModContent.NPCType<BabyGlowmarin>();
+		}
+	}
+}
