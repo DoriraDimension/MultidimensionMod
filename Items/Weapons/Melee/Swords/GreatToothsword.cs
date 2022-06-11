18f75e46a032be851eb479975b137d189092de86
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-06-11 06:18:38 +0200

    Less range, more boner

--- Items/Weapons/Melee/Swords/GreatToothsword.cs
+++ Items/Weapons/Melee/Swords/GreatToothsword.cs
@@ -1,6 +1,8 @@
+using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.DataStructures;
 
 namespace MultidimensionMod.Items.Weapons.Melee.Swords
 {
@@ -9,7 +11,7 @@ namespace MultidimensionMod.Items.Weapons.Melee.Swords
 		public override void SetStaticDefaults() 
 		{
 			DisplayName.SetDefault("Skeletal Greatsword");
-			Tooltip.SetDefault("Ever thought about taking a skeleton and force it into a blade shape, well here you go.");
+			Tooltip.SetDefault("How cruel the world is, humans killing humans for nothing.\nSome are especially cruel and process their victims into weapons, just to show off how many they have killed.\nThe sword throws out bones of the former victims on every swing");
 		}
 
 		public override void SetDefaults()
@@ -20,14 +22,29 @@ namespace MultidimensionMod.Items.Weapons.Melee.Swords
 			Item.height = 82;
 			Item.useTime = 29;
 			Item.useAnimation = 29;
-			Item.useStyle = 1;
+			Item.useStyle = ItemUseStyleID.Swing;
 			Item.knockBack = 4;
 			Item.autoReuse = true;
 			Item.value = Item.sellPrice(gold: 2);
 			Item.rare = ItemRarityID.Lime;
 			Item.UseSound = SoundID.Item1;
-			Item.shoot = ProjectileID.BoneGloveProj;
-			Item.shootSpeed = 10f;
+			Item.shoot = ProjectileID.Bone;
+			Item.shootSpeed = 7f;
+		}
+
+		public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+		{
+			type = ProjectileID.Bone;
+
+			{
+				for (int i = 0; i < 4; i++)
+				{
+					Vector2 newVelocity = velocity.RotatedByRandom(MathHelper.ToRadians(30));
+					newVelocity *= 1f - Main.rand.NextFloat(0.3f);
+					Projectile.NewProjectileDirect(source, position, newVelocity, type, damage, knockback, (int)((double)((float)Item.damage) * 0.3), 0f, player.whoAmI);
+				}
+			}
+			return false;
 		}
 
 		public override void AddRecipes() 
