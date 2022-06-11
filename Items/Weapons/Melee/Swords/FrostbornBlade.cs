df00cbc9bcc479f810f5c183e1b90f6bb0fa3923
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-06-11 07:50:01 +0200

    finally this shitass gets a touchup

--- Items/Weapons/Melee/Swords/FrostbornBlade.cs
+++ Items/Weapons/Melee/Swords/FrostbornBlade.cs
@@ -1,4 +1,6 @@
 using MultidimensionMod.Items.Materials;
+using MultidimensionMod.Projectiles.Melee.Swords;
+using System;
 using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
@@ -12,37 +14,53 @@ namespace MultidimensionMod.Items.Weapons.Melee.Swords
 		public override void SetStaticDefaults() 
 		{
 			DisplayName.SetDefault("Frostborn Blade");
-			Tooltip.SetDefault("Ice created by an elemental from an unknown dimension, shaped into a perfect blade.");
+			Tooltip.SetDefault("A magic sword from another world, a world beyhond the freezing fog of the Frozen Underworld.\nShoots several frost spikes that inflict Frostbite\nDealing critical hits unleashes more frost spikes from the player.");
 		}
 
 		public override void SetDefaults()
 		{
-			Item.CloneDefaults(ItemID.EnchantedSword);
 			Item.damage = 71;
 			Item.DamageType = DamageClass.Melee;
-			Item.width = 70;
-			Item.height = 70;
+			Item.width = 76;
+			Item.height = 76;
+			Item.useTime = 23;
+			Item.useAnimation = 16;
+			Item.useStyle = ItemUseStyleID.Swing;
 			Item.knockBack = 6;
 			Item.autoReuse = true;
 			Item.value = Item.sellPrice(gold: 2);
 			Item.rare = ItemRarityID.Pink;
 			Item.UseSound = SoundID.Item1;
-			Item.crit = 26;
+			Item.shoot = ModContent.ProjectileType<FrostSpike>();
+			Item.shootSpeed = 32f;
+			Item.crit = 9;
 		}
 
 		public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
 		{
-			type = ProjectileID.FrostBoltSword;
+			type = ModContent.ProjectileType<FrostSpike>();
 
 			{
-				for (int i = 0; i < 3; i++)
+				for (int i = 0; i < 2; i++)
 				{
-					Vector2 newVelocity = velocity.RotatedByRandom(MathHelper.ToRadians(20));
+					Vector2 newVelocity = velocity.RotatedByRandom(MathHelper.ToRadians(10));
 					newVelocity *= 1f - Main.rand.NextFloat(0.3f);
 					Projectile.NewProjectileDirect(source, position, newVelocity, type, damage, knockback, player.whoAmI);
 				}
 			}
 			return false;
+		}
+
+		public override void OnHitNPC(Player player, NPC target, int damage, float knockback, bool crit)
+		{
+			if (crit)
+			{
+				for (int i = 0; i < 3; i++)
+				{
+					int spike = Projectile.NewProjectile(Item.GetSource_ItemUse(player.HeldItem), player.Center, new Vector2(Main.rand.NextFloat(-3, 3), Main.rand.NextFloat(-5, -3)), ModContent.ProjectileType<FrostSpike>(), (int)((double)((float)Item.damage) * 0.3), 0f, Main.myPlayer);
+					Main.projectile[spike].aiStyle = 1;
+				}
+			}
 		}
 
 		public override void AddRecipes() 
