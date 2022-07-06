3d2ad53efa6fe96e369d4ce96dc3f8419126fdb6
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-07-06 23:29:41 +0200

    murse cousr

---
+++ Projectiles/Magic/Curse.cs
@@ -0,0 +1,42 @@
+﻿using MultidimensionMod.Dusts;
+using System;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Projectiles.Magic
+{
+	internal class Curse : ModProjectile
+	{
+		public override string Texture => "MultidimensionMod/Projectiles/NoTexture";
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Cursemark");
+		}
+
+		public override void SetDefaults()
+		{
+			Projectile.width = 100;
+			Projectile.height = 100;
+			Projectile.friendly = true;
+			Projectile.DamageType = DamageClass.Magic;
+			Projectile.ignoreWater = true;
+			Projectile.tileCollide = false;
+			Projectile.timeLeft = 5;
+		}
+
+		public override void AI()
+		{
+
+		}
+
+		public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+		{
+			target.AddBuff(BuffID.Slow, 30);
+		}
+	}
+}
