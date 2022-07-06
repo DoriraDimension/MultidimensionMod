60c5a8bc7af222420cac61467f7d1132c5d40b1a
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-07-06 21:06:04 +0200

    sudden dementia

---
+++ Projectiles/Ranged/BrainRocket.cs
@@ -0,0 +1,83 @@
+﻿using MultidimensionMod.Dusts;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.Audio;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Projectiles.Ranged
+{
+	internal class BrainRocket : ModProjectile
+	{
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Brain Rocket");
+		}
+
+		public override void SetDefaults()
+		{
+			Projectile.width = 15;
+			Projectile.height = 15;
+			Projectile.friendly = true;
+			Projectile.DamageType = DamageClass.Ranged;
+			Projectile.ignoreWater = true;
+			Projectile.tileCollide = true;
+			Projectile.hide = false;
+		}
+
+		public override void Kill(int timeLeft)
+		{
+			SoundEngine.PlaySound(SoundID.NPCDeath14, Projectile.position);
+			Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center.X, Projectile.Center.Y + 0f, 0f, 0f, ModContent.ProjectileType<Braindamage>(), (int)((double)((float)Projectile.damage) * 0.8), 0f, Main.myPlayer);
+		}
+
+		public override void AI()
+		{
+			Projectile.direction = Projectile.spriteDirection = Projectile.velocity.X > 0f ? 1 : -1;
+			Projectile.rotation = Projectile.velocity.ToRotation();
+			if (Projectile.velocity.Y > 16f)
+			{
+				Projectile.velocity.Y = 16f;
+			}
+
+			if (Projectile.spriteDirection == -1)
+			{
+				Projectile.rotation += MathHelper.Pi;
+			}
+		}
+	}
+
+	internal class Braindamage : ModProjectile
+	{
+		public override string Texture => "MultidimensionMod/Projectiles/NoTexture";
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Spazzic Explosion");
+		}
+
+		public override void SetDefaults()
+		{
+			Projectile.width = 240;
+			Projectile.height = 240;
+			Projectile.friendly = true;
+			Projectile.DamageType = DamageClass.Ranged;
+			Projectile.ignoreWater = true;
+			Projectile.tileCollide = false;
+			Projectile.penetrate = -1;
+			Projectile.localNPCHitCooldown = -1;
+		}
+
+		public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+		{
+			target.AddBuff(BuffID.Confused, 180);
+		}
+
+		public override void AI()
+		{
+			if (Projectile.timeLeft > 15)
+				Projectile.timeLeft = 15;
+		}
+	}
+}
