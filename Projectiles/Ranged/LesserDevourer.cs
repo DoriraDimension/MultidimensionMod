fcbaa38af193c90acfb2bd4dd532423559d64690
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-07-06 04:33:26 +0200

    mmmm meat

---
+++ Projectiles/Ranged/LesserDevourer.cs
@@ -0,0 +1,49 @@
+﻿using MultidimensionMod.Dusts;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.Audio;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Projectiles.Ranged
+{
+	internal class LesserDevourer : ModProjectile
+	{
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Lesser Devourer");
+		}
+
+		public override void SetDefaults()
+		{
+			Projectile.width = 30;
+			Projectile.height = 30;
+			Projectile.friendly = true;
+			Projectile.DamageType = DamageClass.Ranged;
+			Projectile.ignoreWater = true;
+			Projectile.tileCollide = false;
+			Projectile.hide = false;
+		}
+
+		public override void AI()
+		{
+			Projectile.direction = Projectile.spriteDirection = Projectile.velocity.X > 0f ? 1 : -1;
+			Projectile.rotation = Projectile.velocity.ToRotation();
+			if (Projectile.velocity.Y > 16f)
+			{
+				Projectile.velocity.Y = 16f;
+			}
+
+			if (Projectile.spriteDirection == -1)
+			{
+				Projectile.rotation += MathHelper.Pi;
+			}
+		}
+
+		public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+		{
+			target.AddBuff(BuffID.Poisoned, 240);
+		}
+	}
+}
---
+++ Projectiles/Ranged/RottenSpit.cs
@@ -0,0 +1,58 @@
+﻿using MultidimensionMod.Projectiles.Ranged;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.Audio;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Projectiles.Ranged
+{
+	internal class RottenSpit : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Rotten Spit");
+		}
+
+		public override void SetDefaults()
+		{
+			Projectile.width = 30;
+			Projectile.height = 30;
+			Projectile.friendly = true;
+			Projectile.DamageType = DamageClass.Magic;
+			Projectile.ignoreWater = true;
+			Projectile.tileCollide = true;
+		}
+
+		public override void Kill(int timeLeft)
+		{
+			SoundEngine.PlaySound(SoundID.Item8, Projectile.position);
+
+			for (int i = 0; i < 2; i++)
+			{
+				Gore.NewGore(Projectile.GetSource_FromThis(), Projectile.position, Projectile.velocity, ModContent.Find<ModGore>("MultidimensionMod/RottenGore3").Type, 1);
+			}
+			for (int i = 0; i < 2; i++)
+			{
+				Gore.NewGore(Projectile.GetSource_FromThis(), Projectile.position, Projectile.velocity, ModContent.Find<ModGore>("MultidimensionMod/RottenGore2").Type, 1);
+			}
+			for (int i = 0; i < 6; i++)
+			{
+				Gore.NewGore(Projectile.GetSource_FromThis(), Projectile.position, Projectile.velocity, ModContent.Find<ModGore>("MultidimensionMod/RottenGore1").Type, 1);
+			}
+		}
+
+		public override void AI()
+		{
+			if (Projectile.timeLeft > 180)
+				Projectile.timeLeft = 180;
+			Projectile.rotation += 0.2f * (float)Projectile.direction;
+			Projectile.velocity *= 0.85f;
+
+			if (Projectile.timeLeft % 20 == 19 && Projectile.owner == Main.myPlayer)
+			{
+				Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center.X + (float)Main.rand.Next(-100, 100), Projectile.Center.Y + 600f, 0f, -15f, ModContent.ProjectileType<LesserDevourer>(), (int)((double)((float)Projectile.damage) * 0.7), 0f, Projectile.owner);
+			}
+		}
+	}
+}
