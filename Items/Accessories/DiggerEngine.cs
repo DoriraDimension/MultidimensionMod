549b3fd57575819d5cbd112d51d577a1cb5e43ee
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-07-14 06:43:16 +0200

    diggah

---
+++ Items/Accessories/DiggerEngine.cs
@@ -0,0 +1,55 @@
+﻿using MultidimensionMod.Items.Souls;
+using MultidimensionMod.Items.Materials;
+using MultidimensionMod.Tiles;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Items.Accessories
+{
+	public class DiggerEngine : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Digger Engine");
+			Tooltip.SetDefault("A excavator engine used in the Destroyer to mine minerals incredibly fast.\nIncreases mining speed by 100% and spawns friendly Probes when you get hit.");
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 26;
+			Item.height = 42;
+			Item.accessory = true;
+			Item.value = Item.sellPrice(gold: 12);
+			Item.rare = ItemRarityID.Pink;
+		}
+
+		public override void ModifyTooltips(List<TooltipLine> list)
+		{
+			foreach (TooltipLine Item in list)
+			{
+				if (Item.Mod == "Terraria" && Item.Name == "ItemName")
+				{
+					Item.OverrideColor = MDRarity.BossItem;
+				}
+			}
+		}
+
+		public override void UpdateAccessory(Player player, bool hideVisual)
+		{
+			player.pickSpeed += 0.50f;
+			player.GetModPlayer<MDPlayer>().Probe = true;
+		}
+
+		public override void AddRecipes()
+		{
+			CreateRecipe()
+			.AddIngredient(ModContent.ItemType<MetalWormSoul>())
+			.AddIngredient(ModContent.ItemType<Dimensium>(), 15)
+			.AddIngredient(ModContent.TileType<DimensionalForge>())
+			.AddTile(134)
+			.Register();
+		}
+	}
+}
