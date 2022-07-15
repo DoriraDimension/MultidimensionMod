32486313155ff5128bf119bd7154f34440b00b05
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-07-15 17:20:37 +0200

    fuck you

---
+++ MDConditions.cs
@@ -0,0 +1,18 @@
+﻿using Terraria.GameContent.ItemDropRules;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod
+{
+	public class SlimeCondition : IItemDropRuleCondition
+	{
+		public bool CanDrop(DropAttemptInfo info)
+		{
+			if (!info.IsInSimulation)
+				return !NPC.downedSlimeKing;
+			return false;
+		}
+		public bool CanShowItemDropInUI() => true;
+		public string GetConditionDescription() => "Dropped once per world";
+	}
+}
