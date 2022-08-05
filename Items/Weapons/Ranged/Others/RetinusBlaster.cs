14b9da78fd07279d55032e201f5f9c4efd902181
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-08-05 09:25:45 +0200

    Retinus Blester

---
+++ Items/Weapons/Ranged/Others/RetinusBlaster.cs
@@ -0,0 +1,67 @@
+﻿using MultidimensionMod.Items.Materials;
+using MultidimensionMod.Items.Souls;
+using MultidimensionMod.Tiles;
+using MultidimensionMod.Projectiles.Ranged;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Items.Weapons.Ranged.Others
+{
+	public class RetinusBlaster : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Retinus Blaster");
+			Tooltip.SetDefault("Charges a devastating death beam");
+		}
+
+		public override void SetDefaults()
+		{
+			Item.DamageType = DamageClass.Ranged;
+			Item.damage = 120;
+			Item.useStyle = ItemUseStyleID.Shoot;
+			Item.useTime = 40;
+			Item.useAnimation = 40;
+			Item.knockBack = 0f;
+			Item.UseSound = SoundID.Item8;
+			Item.width = 80;
+			Item.height = 38;
+			Item.value = Item.sellPrice(gold: 1);
+			Item.autoReuse = true;
+			Item.noMelee = true;
+			Item.noUseGraphic = true;
+			Item.channel = true;
+			Item.rare = ItemRarityID.Pink;
+			Item.shoot = ModContent.ProjectileType<RetinusBlasterHoldOut>();
+			Item.shootSpeed = 15f;
+		}
+
+		public override void ModifyTooltips(List<TooltipLine> list)
+		{
+			foreach (TooltipLine Item in list)
+			{
+				if (Item.Mod == "Terraria" && Item.Name == "ItemName")
+				{
+					Item.OverrideColor = MDRarity.BossItem;
+				}
+			}
+		}
+
+		public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+		{
+			return player.ownedProjectileCounts[Item.shoot] < 6;
+		}
+		public override void AddRecipes()
+		{
+			CreateRecipe()
+			.AddIngredient(ModContent.ItemType<TwinSoul>())
+			.AddIngredient(ModContent.ItemType<Dimensium>(), 15)
+			.AddTile(ModContent.TileType<DimensionalForge>())
+			.Register();
+		}
+	}
+}
