8d0ac03d54dd7a0611590098b1b6b6bbf8ad9fb5
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-08-30 00:54:45 +0200

    Madness fragment

---
+++ Items/Materials/MadnessFragment.cs
@@ -0,0 +1,25 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Items.Materials
+{
+	public class MadnessFragment : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Fragment of Madness");
+			Tooltip.SetDefault("A fragment of a fragment LOL.");
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 24;
+			Item.height = 30;
+			Item.maxStack = 9999;
+			Item.value = Item.sellPrice(copper: 8);
+			Item.rare = ItemRarityID.Green;
+		}
+	}
+}
