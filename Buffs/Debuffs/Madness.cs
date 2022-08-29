086ff4fb46fba95ad1989cfa2b93f1c18c3514c6
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-08-30 00:55:31 +0200

    fuck this honestly

---
+++ Buffs/Debuffs/Madness.cs
@@ -0,0 +1,25 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Buffs.Debuffs
+{
+    public class Madness : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Madness");
+            Description.SetDefault("you suck and just realized it.");
+            Main.buffNoTimeDisplay[Type] = false;
+            Main.debuff[Type] = false;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.GetModPlayer<MDPlayer>().Madness = true;
+        }
+
+        public override void Update(NPC npc, ref int buffIndex)
+        {
+        }
+    }
+}
---
+++ Buffs/Potions/CalmMind.cs
@@ -0,0 +1,23 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Buffs.Potions
+{
+    public class CalmMind : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Calm Mind");
+            Description.SetDefault("Madness has a harder time reaching your mind.");
+            Main.buffNoTimeDisplay[Type] = false;
+            Main.debuff[Type] = false;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.GetModPlayer<MDPlayer>().CalmMind = true;
+        }
+    }
+}
