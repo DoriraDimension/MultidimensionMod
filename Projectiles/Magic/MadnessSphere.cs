2e22bd42c11f3fe8206c14f29a76fee8ea960c4b
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-08-30 19:28:41 +0200

    madness sphäre

---
+++ Projectiles/Magic/MadnessSphere.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.Audio;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Projectiles.Magic
+{
+	internal class MadnessSphere : ModProjectile
+	{
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Madness Sphere");
+		}
+
+		public override void SetDefaults()
+		{
+			Projectile.width = 20;
+			Projectile.height = 20;
+			Projectile.friendly = true;
+			Projectile.DamageType = DamageClass.Magic;
+			Projectile.ignoreWater = true;
+			Projectile.tileCollide = true;
+			Projectile.hide = false;
+			Projectile.penetrate = 3;
+		}
+
+		public override void AI()
+		{
+			Projectile.velocity.Y = Projectile.velocity.Y + 0.2f;
+			if (Projectile.velocity.Y > 16f)
+			{
+				Projectile.velocity.Y = 16f;
+			}
+			Projectile.rotation += 0.4f * (float)Projectile.direction;
+		}
+
+		public override void ModifyHitNPC(NPC target, ref int damage, ref float knockback, ref bool crit, ref int hitDirection)
+		{
+			Projectile.damage = (int)((double)Projectile.damage * 1.25);
+		}
+
+		public override bool OnTileCollide(Vector2 oldVelocity)
+		{
+			Projectile.damage = (int)((double)Projectile.damage * 1.25);
+			Projectile.penetrate--;
+			if (Projectile.penetrate <= 0)
+			{
+				Projectile.Kill();
+			}
+			else
+			{
+				Collision.HitTiles(Projectile.position + Projectile.velocity, Projectile.velocity, Projectile.width, Projectile.height);
+				SoundEngine.PlaySound(SoundID.Item10, Projectile.position);
+				if (Projectile.velocity.X != oldVelocity.X)
+				{
+					Projectile.velocity.X = -oldVelocity.X;
+				}
+				if (Projectile.velocity.Y != oldVelocity.Y)
+				{
+					Projectile.velocity.Y = -oldVelocity.Y;
+				}
+			}
+			return false;
+		}
+	}
+}
