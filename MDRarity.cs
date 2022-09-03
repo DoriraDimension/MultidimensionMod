63ed7e7be138755f33ab0e4fe663dbbd94d7e03d
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-09-03 19:21:08 +0200

    forgot to rename parallel rarity

--- MDRarity.cs
+++ MDRarity.cs
@@ -14,7 +14,7 @@ namespace MultidimensionMod
 	{
 		public static Color RelicWeapon => new Color(125, 65, 47);
 
-		public static Color ParallelVariant => new Color(95, 57, 249);
+		public static Color ForbiddenArtifact => new Color(95, 57, 249);
 
 		public static Color Dorira => new Color(18, 279, 247);
 
