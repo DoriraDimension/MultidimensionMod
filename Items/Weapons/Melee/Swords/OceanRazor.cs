e365d0a5e63b44c3eccd512985363e80afb1fcb3
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-09-06 00:35:24 +0200

    Spear Willy

---
+++ Items/Weapons/Melee/Swords/OceanRazor.cs
@@ -0,0 +1,55 @@
+﻿using MultidimensionMod.Projectiles.Melee.Swords;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Items.Weapons.Melee.Swords
+{
+	public class OceanRazor : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Ocean Razor");
+			Tooltip.SetDefault("This is definetily a knife with water thats salty.");
+		}
+
+		public override void SetDefaults()
+		{
+			Item.damage = 24;
+			Item.DamageType = DamageClass.Melee;
+			Item.width = 48;
+			Item.height = 48;
+			Item.useTime = 13;
+			Item.useAnimation = 13;
+			Item.useStyle = ItemUseStyleID.Rapier;
+			Item.knockBack = 1.5f;
+			Item.value = Item.sellPrice(copper: 45);
+			Item.rare = ItemRarityID.Blue;
+			Item.UseSound = SoundID.Item1;
+			Item.autoReuse = true;
+			Item.noMelee = true;
+			Item.noUseGraphic = true;
+			Item.shoot = ModContent.ProjectileType<OceanRazorStab>();
+			Item.shootSpeed = 4f;
+		}
+
+		public override void AddRecipes()
+		{
+			CreateRecipe()
+			.AddIngredient(ItemID.IronShortsword)
+			.AddIngredient(ItemID.Starfish, 3)
+			.AddIngredient(ItemID.Coral, 7)
+			.AddIngredient(ItemID.GoldBar, 2)
+			.AddTile(TileID.Anvils)
+			.Register();
+
+			CreateRecipe()
+            .AddIngredient(ItemID.LeadShortsword)
+            .AddIngredient(ItemID.Starfish, 3)
+            .AddIngredient(ItemID.Coral, 7)
+            .AddIngredient(ItemID.GoldBar, 2)
+            .AddTile(TileID.Anvils)
+            .Register();
+		}
+	}
+}
