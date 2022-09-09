a5ff6e862b0fb828368188c6c7a3f19fca7aaa86
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-09-09 05:56:44 +0200

    scythies

---
+++ Projectiles/Melee/Swords/AbyssalSoul.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.Audio;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Projectiles.Melee.Swords
+{
+	internal class AbyssalSoul : ModProjectile
+	{
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Abyssal Soul");
+		}
+
+		public override void SetDefaults()
+		{
+			Projectile.width = 18;
+			Projectile.height = 18;
+			Projectile.friendly = true;
+			Projectile.DamageType = DamageClass.Melee;
+			Projectile.tileCollide = true;
+		}
+
+		public override void Kill(int timeLeft)
+		{
+			for (int i = 0; i < 3; i++)
+			{
+				int dustIndex = Dust.NewDust(new Vector2(Projectile.position.X, Projectile.position.Y), Projectile.width, Projectile.height, DustID.Smoke, 0f, 0f, 100, default(Color), 2f);
+				Main.dust[dustIndex].velocity *= 1.4f;
+			}
+		}
+
+		public override void AI()
+		{
+			if (Projectile.timeLeft > 180)
+				Projectile.timeLeft = 180;
+
+			Player player = Main.player[Projectile.owner];
+			float distanceFromTarget = 700f;
+			Vector2 targetCenter = Projectile.position;
+			bool foundTarget = false;
+
+			if (player.HasMinionAttackTargetNPC)
+			{
+				NPC npc = Main.npc[player.MinionAttackTargetNPC];
+				float between = Vector2.Distance(npc.Center, Projectile.Center);
+				if (between < 2000f)
+				{
+					distanceFromTarget = between;
+					targetCenter = npc.Center;
+					foundTarget = true;
+				}
+			}
+			if (!foundTarget)
+			{
+				for (int i = 0; i < Main.maxNPCs; i++)
+				{
+					NPC npc = Main.npc[i];
+					if (npc.CanBeChasedBy())
+					{
+						float between = Vector2.Distance(npc.Center, Projectile.Center);
+						bool closest = Vector2.Distance(Projectile.Center, targetCenter) > between;
+						bool inRange = between < distanceFromTarget;
+						bool lineOfSight = Collision.CanHitLine(Projectile.position, Projectile.width, Projectile.height, npc.position, npc.width, npc.height);
+						bool closeThroughWall = between < 100f;
+						if (((closest && inRange) || !foundTarget) && (lineOfSight || closeThroughWall))
+						{
+							distanceFromTarget = between;
+							targetCenter = npc.Center;
+							foundTarget = true;
+						}
+					}
+				}
+			}
+
+			Projectile.friendly = foundTarget;
+
+			float speed = 8f;
+			float inertia = 20f;
+
+			if (foundTarget)
+			{
+				if (distanceFromTarget > 40f)
+				{
+					Vector2 direction = targetCenter - Projectile.Center;
+					direction.Normalize();
+					direction *= speed;
+					Projectile.velocity = (Projectile.velocity * (inertia - 1) + direction) / inertia;
+				}
+			}
+			else
+			{
+				if (!foundTarget)
+				{
+					speed = 4f;
+					inertia = 60f;
+				}
+				else
+				{
+					speed = 8f;
+					inertia = 300f;
+				}
+
+				if (Projectile.spriteDirection == -1)
+				{
+					Projectile.rotation += MathHelper.Pi;
+				}
+			}
+
+			Projectile.direction = Projectile.spriteDirection = Projectile.velocity.X > 0f ? 1 : -1;
+			Projectile.rotation = Projectile.velocity.ToRotation();
+			if (Projectile.velocity.Y > 16f)
+			{
+				Projectile.velocity.Y = 16f;
+			}
+			if (Projectile.spriteDirection == -1)
+			{
+				Projectile.rotation += MathHelper.Pi;
+			}
+		}
+	}
+}
---
+++ Projectiles/Melee/Swords/HiddenSunScythe.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.Audio;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Projectiles.Melee.Swords
+{
+	internal class HiddenSunScythe : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Hidden Sun Scythe");
+		}
+
+		public override void SetDefaults()
+		{
+			Projectile.width = 66;
+			Projectile.height = 66;
+			Projectile.friendly = true;
+			Projectile.DamageType = DamageClass.Melee;
+			Projectile.ignoreWater = true;
+			Projectile.tileCollide = false;
+			Projectile.penetrate = -1;
+		}
+
+		public override void Kill(int timeLeft)
+		{
+			for (int i = 0; i < 5; i++)
+			{
+				int dustIndex = Dust.NewDust(new Vector2(Projectile.position.X, Projectile.position.Y), Projectile.width, Projectile.height, DustID.Smoke, 0f, 0f, 100, default(Color), 2f);
+				Main.dust[dustIndex].velocity *= 1.4f;
+			}
+			for (int e = 0; e < 5; e++)
+			{
+				Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, new Vector2(Main.rand.NextFloat(-3, 3) * 2, Main.rand.NextFloat(-9, 10)) * 2, ModContent.ProjectileType<AbyssalSoul>(), (int)((double)((float)Projectile.damage) * 0.2f), 0f, Main.myPlayer);
+			}
+		}
+
+		public override void AI()
+		{
+			if (Projectile.timeLeft > 180)
+				Projectile.timeLeft = 180;			
+			Projectile.spriteDirection = Projectile.direction;
+			Projectile.rotation += 0.6f * (float)Projectile.direction;
+            Projectile.velocity *= 0.98f;
+
+			if (Projectile.timeLeft % 20 == 19 && Projectile.owner == Main.myPlayer)
+			{
+				Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, new Vector2(Main.rand.NextFloat(-10, 10), Main.rand.NextFloat(-9, 10)), ModContent.ProjectileType<AbyssalSoul>(), (int)((double)((float)Projectile.damage) * 0.2), 0f, Projectile.owner);
+			}
+		}
+	}
+}
