863709f08bc50bed4a5ad25b0106673f512f22a1
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-09-09 09:57:32 +0200

    bow resproot and water bolts

--- Items/Weapons/Ranged/Bows/ToxotesArcus.cs
+++ Items/Weapons/Ranged/Bows/ToxotesArcus.cs
@@ -9,6 +9,7 @@ namespace MultidimensionMod.Items.Weapons.Ranged.Bows
 {
 	public class ToxotesArcus : ModItem
 	{
+		public int WaveShot = 0;
 		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Toxotes Arcus");
@@ -20,7 +21,7 @@ namespace MultidimensionMod.Items.Weapons.Ranged.Bows
 			Item.damage = 54;
 			Item.DamageType = DamageClass.Ranged;
 			Item.width = 28;
-			Item.height = 46;
+			Item.height = 24;
 			Item.useTime = 5;
 			Item.reuseDelay = 20;
 			Item.useAnimation = 20;
@@ -43,12 +44,37 @@ namespace MultidimensionMod.Items.Weapons.Ranged.Bows
 			float speedY2 = velocity.Y * 1.0f;
 			int nah = Projectile.NewProjectile(source, position.X, position.Y, velocity.X, velocity.Y, type, damage, knockback, player.whoAmI);
 			Main.projectile[nah].noDropItem = true;
+
+			if (WaveShot == 3)
+            {
+				float rotation = MathHelper.ToRadians(15);
+
+				position += Vector2.Normalize(velocity) * 45f;
+
+				for (int i = 0; i < 2; i++)
+				{
+					Vector2 perturbedSpeed = velocity.RotatedBy(MathHelper.Lerp(-rotation, rotation, i / (2 - 1))) * .2f;
+					Projectile.NewProjectile(source, position, perturbedSpeed, ProjectileID.WaterBolt, damage, knockback, player.whoAmI);
+				}
+				return false;
+			}
 			return false;
+		}
+
+		public override bool? UseItem(Player player)
+		{
+			WaveShot += 1;
+			if (WaveShot >= 4)
+			{
+				WaveShot = 0;
+			}
+			return true;
 		}
 
 		public override void AddRecipes()
 		{
 			CreateRecipe()
+			.AddIngredient(ModContent.ItemType<CoralBow>())
 			.AddIngredient(ModContent.ItemType<TidalQuartz>(), 6)
 			.AddIngredient(ItemID.HallowedBar, 13)
 			.AddTile(134)
