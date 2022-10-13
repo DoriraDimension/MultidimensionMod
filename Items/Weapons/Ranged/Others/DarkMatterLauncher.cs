c148d44e7028c4024c0e08f4667c42e597303ac3
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-10-13 19:14:13 +0200

    moved these two to "others"

---
+++ Items/Weapons/Ranged/Others/DarkMatterLauncher.cs
@@ -0,0 +1,43 @@
+﻿using MultidimensionMod.Projectiles.Ranged;
+using Terraria;
+using Microsoft.Xna.Framework;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Items.Weapons.Ranged.Others
+{
+	public class DarkMatterLauncher : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Dark Matter Launcher");
+			Tooltip.SetDefault("You could basically shove the boss into this thing to use as ammo.");
+		}
+
+		public override void SetDefaults()
+		{
+			Item.damage = 44;
+			Item.DamageType = DamageClass.Ranged;
+			Item.width = 54;
+			Item.height = 26;
+			Item.useTime = 50;
+			Item.useAnimation = 50;
+			Item.useStyle = ItemUseStyleID.Shoot;
+			Item.noMelee = true;
+			Item.knockBack = 4;
+			Item.value = Item.sellPrice(gold: 1);
+			Item.rare = ItemRarityID.LightRed;
+			Item.UseSound = SoundID.Item11;
+			Item.autoReuse = true;
+			Item.shoot = ModContent.ProjectileType<DarkMatterBlob>();
+			Item.shootSpeed = 9f;
+			Item.crit = 8;
+		}
+
+		public override Vector2? HoldoutOffset()
+		{
+			return new Vector2(-10, 0);
+		}
+	}
+}
---
+++ Items/Weapons/Ranged/Others/RotSpit.cs
@@ -0,0 +1,67 @@
+﻿using MultidimensionMod.Projectiles.Ranged;
+using MultidimensionMod.Items.Souls;
+using MultidimensionMod.Items.Materials;
+using MultidimensionMod.Tiles;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Items.Weapons.Ranged.Others
+{
+	public class RotSpit : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Rot Spit");
+			Tooltip.SetDefault("Spits out a big ball of rotten flesh that attracts small devourers. Devourer will poison enemies on hit.");
+		}
+
+		public override void SetDefaults()
+		{
+			Item.damage = 50;
+			Item.DamageType = DamageClass.Ranged;
+			Item.width = 86;
+			Item.height = 24;
+			Item.useTime = 90;
+			Item.useAnimation = 90;
+			Item.useStyle = ItemUseStyleID.Shoot;
+			Item.noMelee = true;
+			Item.knockBack = 1;
+			Item.value = Item.sellPrice(silver: 26);
+			Item.rare = ItemRarityID.Green;
+			Item.UseSound = SoundID.NPCDeath13;
+			Item.autoReuse = true;
+			Item.shoot = ModContent.ProjectileType<RottenSpit>();
+			Item.shootSpeed = 65f;
+			Item.crit = 13;
+		}
+
+		public override void ModifyTooltips(List<TooltipLine> list)
+		{
+			foreach (TooltipLine Item in list)
+			{
+				if (Item.Mod == "Terraria" && Item.Name == "ItemName")
+				{
+					Item.OverrideColor = MDRarity.BossItem;
+				}
+			}
+		}
+
+		public override Vector2? HoldoutOffset()
+		{
+			return new Vector2(-10, 0);
+		}
+
+		public override void AddRecipes()
+		{
+			CreateRecipe()
+			.AddIngredient(ModContent.ItemType<WormSoul>())
+			.AddIngredient(ModContent.ItemType<Dimensium>(), 10)
+			.AddTile(ModContent.TileType<DimensionalForge>())
+			.Register();
+		}
+	}
+}
