67fb270cc7e6d88322a1f71ea47393154c015bff
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-10-29 03:13:55 +0200

    whip projectos

---
+++ Projectiles/Summon/Whips/MadnessWhipProj.cs
@@ -0,0 +1,105 @@
+﻿using MultidimensionMod.Buffs.Debuffs;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Projectiles.Summon.Whips
+{
+	public class MadnessWhipProj : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			ProjectileID.Sets.IsAWhip[Type] = true;
+		}
+
+		public override void SetDefaults()
+		{
+			Projectile.DefaultToWhip();
+		}
+
+		public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+		{
+			target.AddBuff(ModContent.BuffType<Madness>(), 120);
+			Main.player[Projectile.owner].MinionAttackTargetNPC = target.whoAmI;
+		}
+
+		private void DrawLine(List<Vector2> list)
+		{
+			Texture2D texture = TextureAssets.FishingLine.Value;
+			Rectangle frame = texture.Frame();
+			Vector2 origin = new Vector2(frame.Width / 2, 2);
+
+			Vector2 pos = list[0];
+			for (int i = 0; i < list.Count - 1; i++)
+			{
+				Vector2 element = list[i];
+				Vector2 diff = list[i + 1] - element;
+
+				float rotation = diff.ToRotation() - MathHelper.PiOver2;
+				Color color = Lighting.GetColor(element.ToTileCoordinates(), Color.White);
+				Vector2 scale = new Vector2(1, (diff.Length() + 2) / frame.Height);
+
+				Main.EntitySpriteDraw(texture, pos - Main.screenPosition, frame, color, rotation, origin, scale, SpriteEffects.None, 0);
+
+				pos += diff;
+			}
+		}
+
+		public override bool PreDraw(ref Color lightColor)
+		{
+			List<Vector2> list = new List<Vector2>();
+			Projectile.FillWhipControlPoints(Projectile, list);
+
+			DrawLine(list);
+
+			SpriteEffects flip = Projectile.spriteDirection < 0 ? SpriteEffects.None : SpriteEffects.FlipHorizontally;
+
+			Main.instance.LoadProjectile(Type);
+			Texture2D texture = TextureAssets.Projectile[Type].Value;
+
+			Vector2 pos = list[0];
+
+			for (int i = 0; i < list.Count - 1; i++)
+			{
+				Rectangle frame = new Rectangle(0, 0, 16, 26);
+				Vector2 origin = new Vector2(8, 8);
+				float scale = 1;
+
+				if (i == list.Count - 2)
+				{
+					frame.Y = 62;
+					frame.Height = 20;
+
+					Projectile.GetWhipSettings(Projectile, out float timeToFlyOut, out int _, out float _);
+					float t = timeToFlyOut;
+					scale = MathHelper.Lerp(0.8f, 1.2f, Utils.GetLerpValue(1.0f, 1.2f, t, true) * Utils.GetLerpValue(1.2f, 1.3f, t, true));
+				}
+				else if (i > 10)
+				{
+					frame.Y = 50;
+					frame.Height = 10;
+				}
+				else if (i > 0)
+				{
+					frame.Y = 34;
+					frame.Height = 14;
+				}
+
+				Vector2 element = list[i];
+				Vector2 diff = list[i + 1] - element;
+
+				float rotation = diff.ToRotation() - MathHelper.PiOver2;
+				Color color = Lighting.GetColor(element.ToTileCoordinates());
+
+				Main.EntitySpriteDraw(texture, pos - Main.screenPosition, frame, color, rotation, origin, scale, flip, 0);
+
+				pos += diff;
+			}
+			return false;
+		}
+	}
+}
