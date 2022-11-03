932a583b8b4a33f1fc8030196e7a4273700314c0
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-11-03 19:37:46 +0100

    brick bomb

---
+++ Projectiles/Ranged/CompletelyOutOfPlaceExplosionGif.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Projectiles.Ranged
+{
+	internal class CompletelyOutOfPlaceExplosionGif : ModProjectile
+	{
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Explosion gif that was ripped from google");
+			Main.projFrames[Projectile.type] = 14;
+		}
+
+		public override void SetDefaults()
+		{
+			Projectile.width = 150;
+			Projectile.height = 150;
+			Projectile.friendly = true;
+			Projectile.DamageType = DamageClass.Ranged;
+			Projectile.ignoreWater = true;
+			Projectile.tileCollide = false;
+			Projectile.penetrate = -1;
+			Projectile.localNPCHitCooldown = -1;
+		}
+
+		public override void AI()
+		{
+			if (Projectile.timeLeft > 70)
+				Projectile.timeLeft = 70;
+
+			if (++Projectile.frameCounter >= 5)
+			{
+				Projectile.frameCounter = 0;
+				if (++Projectile.frame >= 14)
+				{
+					Projectile.frame = 0;
+				}
+			}
+		}
+	}
+}
---
+++ Projectiles/Ranged/SunrayBrickThrown.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.Audio;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Projectiles.Ranged
+{
+	internal class SunrayBrickThrown : ModProjectile
+	{
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Nuclear Bomb");
+		}
+
+		public override void SetDefaults()
+		{
+			Projectile.width = 30;
+			Projectile.height = 30;
+			Projectile.friendly = true;
+			Projectile.DamageType = DamageClass.Ranged;
+			Projectile.ignoreWater = false;
+			Projectile.tileCollide = true;
+			Projectile.hide = false;
+		}
+
+		public override void AI()
+		{
+			Projectile.rotation += 0.2f * (float)Projectile.direction;
+		}
+
+		public override void Kill(int timeLeft)
+		{
+			SoundEngine.PlaySound(new("MultidimensionMod/Sounds/Custom/FireExplosion"), Projectile.position);
+
+			for (int i = 0; i < 50; i++)
+			{
+				Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center.X, Projectile.Center.Y + 0f, 0f, 0f, ModContent.ProjectileType<CompletelyOutOfPlaceExplosionGif>(), (int)((double)((float)Projectile.damage) * 0.5), 0f, Main.myPlayer);
+			}
+		}
+	}
+}
