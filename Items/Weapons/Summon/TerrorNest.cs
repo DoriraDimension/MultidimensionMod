fcada39ac4b4311052e62939a29efbfeac0cbefd
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-11-04 13:16:47 +0100

    freakin death bee

---
+++ Items/Weapons/Summon/TerrorNest.cs
@@ -0,0 +1,53 @@
+﻿using MultidimensionMod.Buffs.Minions;
+using MultidimensionMod.Projectiles.Summon.Minions;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Items.Weapons.Summon
+{
+	public class TerrorNest : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Terror Nest");
+			Tooltip.SetDefault("Summons a killer bee that grows sawblades when enemies are spotted.\nA rare bee species, adapted to hunt and kill prey.\nTheir honey contains special pollen that gives it a bloodred color, totally not the blood of their victims...");
+			ItemID.Sets.GamepadWholeScreenUseRange[Item.type] = true;
+			ItemID.Sets.LockOnIgnoresCollision[Item.type] = true;
+		}
+
+		public override void SetDefaults()
+		{
+			Item.damage = 42;
+			Item.DamageType = DamageClass.Summon;
+			Item.knockBack = 1f;
+			Item.mana = 10;
+			Item.width = 30;
+			Item.height = 32;
+			Item.useTime = 36;
+			Item.useAnimation = 36;
+			Item.useStyle = ItemUseStyleID.Shoot;
+			Item.value = Item.sellPrice(gold: 4);
+			Item.rare = ItemRarityID.LightRed;
+			Item.UseSound = SoundID.NPCHit54;
+			Item.noMelee = true;
+			Item.buffType = ModContent.BuffType<FleshrendingTerror>();
+			Item.shoot = ModContent.ProjectileType<TerrorBee>();
+		}
+
+		public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+		{
+			player.AddBuff(Item.buffType, 2);
+			var projectile = Projectile.NewProjectileDirect(source, position, velocity, type, damage, knockback, Main.myPlayer);
+			projectile.originalDamage = Item.damage;
+			return false;
+		}
+
+		public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+		{
+			position = Main.MouseWorld;
+		}
+	}
+}
