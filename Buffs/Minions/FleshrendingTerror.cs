f94180737ae8b9d4e9dc9bb08dc3ce323deb0bb0
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-11-04 13:17:53 +0100

    flesh shall be consumed

---
+++ Buffs/Minions/FleshrendingTerror.cs
@@ -0,0 +1,30 @@
+﻿using MultidimensionMod.Projectiles.Summon.Minions;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Buffs.Minions
+{
+	public class FleshrendingTerror : ModBuff
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Terror Bee");
+			Description.SetDefault("A malicious hunter is fighting for you");
+			Main.buffNoSave[Type] = true;
+			Main.buffNoTimeDisplay[Type] = true;
+		}
+
+		public override void Update(Player player, ref int buffIndex)
+		{
+			if (player.ownedProjectileCounts[ModContent.ProjectileType<TerrorBee>()] > 0)
+			{
+				player.buffTime[buffIndex] = 18000;
+			}
+			else
+			{
+				player.DelBuff(buffIndex);
+				buffIndex--;
+			}
+		}
+	}
+}
