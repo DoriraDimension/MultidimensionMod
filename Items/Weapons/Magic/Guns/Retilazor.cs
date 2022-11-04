2039c6f558849cf3e48fd6b36840f402c4e79c21
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-11-04 15:45:24 +0100

    lower volume part 1

--- Items/Weapons/Magic/Guns/Retilazor.cs
+++ Items/Weapons/Magic/Guns/Retilazor.cs
@@ -1,7 +1,10 @@
 ﻿using MultidimensionMod.Projectiles.Magic;
+using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.Audio;
+using Terraria.DataStructures;
 
 namespace MultidimensionMod.Items.Weapons.Magic.Guns
 {
@@ -26,12 +29,17 @@ namespace MultidimensionMod.Items.Weapons.Magic.Guns
 			Item.noMelee = true;
 			Item.value = Item.sellPrice(gold: 3);
 			Item.rare = ItemRarityID.Pink;
-			Item.UseSound = SoundID.Item33;
 			Item.mana = 11;
 			Item.autoReuse = true;
 			Item.shoot = ModContent.ProjectileType<RedLazor>();
 			Item.shootSpeed = 35f;
 			Item.crit = 4;
+		}
+
+		public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+		{
+			SoundEngine.PlaySound(SoundID.Item33 with {Volume = 0.4f}, player.position);
+			return true;
 		}
 	}
 }
