d89669449b6f6964c3dcf49525568a1cb72cf0b1
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-11-05 17:48:14 +0100

    Im vikinging

---
+++ Items/Materials/VikingRelic.cs
@@ -0,0 +1,25 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Items.Materials
+{
+	public class VikingRelic : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Viking Relic");
+			Tooltip.SetDefault("A piece of icy history.");
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 18;
+			Item.height = 18;
+			Item.maxStack = 9999;
+			Item.value = Item.sellPrice(copper: 46);
+			Item.rare = ItemRarityID.Green;
+		}
+	}
+}
