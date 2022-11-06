a1bc03294d6a5df76a8a742ccc0fe71bccbf10d9
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-11-06 06:25:00 +0100

    and their projecties

---
+++ Projectiles/Melee/Spears/DuskCrescent.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.Audio;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Projectiles.Melee.Spears
+{
+	internal class DuskCrescent : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Dusk Crescent");
+		}
+
+		public override void SetDefaults()
+		{
+			Projectile.width = 22;
+			Projectile.height = 22;
+			Projectile.friendly = true;
+			Projectile.DamageType = DamageClass.Melee;
+			Projectile.ignoreWater = true;
+			Projectile.tileCollide = false;
+			Projectile.penetrate = -1;
+		}
+
+		public override void Kill(int timeLeft)
+		{
+
+		}
+
+		public override void AI()
+		{
+			if (Projectile.timeLeft > 180)
+				Projectile.timeLeft = 180;
+			Projectile.spriteDirection = Projectile.direction;
+			Projectile.rotation += 0.6f * (float)Projectile.direction;
+			Projectile.velocity *= 0.98f;
+		}
+	}
+}
---
+++ Projectiles/Melee/Spears/JungleLeaf.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.Audio;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Projectiles.Melee.Spears
+{
+	internal class JungleLeaf : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Jungle Leaf");
+		}
+
+		public override void SetDefaults()
+		{
+			Projectile.width = 10;
+			Projectile.height = 10;
+			Projectile.friendly = true;
+			Projectile.DamageType = DamageClass.Melee;
+			Projectile.ignoreWater = true;
+			Projectile.tileCollide = false;
+			Projectile.penetrate = -1;
+		}
+
+		public override void Kill(int timeLeft)
+		{
+
+		}
+
+		public override void AI()
+		{
+			if (Projectile.timeLeft > 60)
+				Projectile.timeLeft = 60;
+
+			Projectile.direction = Projectile.spriteDirection = Projectile.velocity.X > 0f ? 1 : -1;
+			Projectile.rotation = Projectile.velocity.ToRotation();
+			if (Projectile.spriteDirection == -1)
+			{
+				Projectile.rotation += MathHelper.Pi;
+			}
+		}
+	}
+}
