4c0d98c42dad95706045b6b8dd631a8674c28929
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-11-13 22:23:52 +0100

    clump resprite

--- Items/Materials/DarkMatterClump.cs
+++ Items/Materials/DarkMatterClump.cs
@@ -1,6 +1,5 @@
 using Terraria;
 using Terraria.ID;
-using Terraria.Localization;
 using Terraria.ModLoader;
 
 namespace MultidimensionMod.Items.Materials
@@ -9,14 +8,14 @@ namespace MultidimensionMod.Items.Materials
 	{
 		public override void SetStaticDefaults() 
 		{
-			DisplayName.SetDefault("Dark Matter Clump");
-			Tooltip.SetDefault("The essence of Darklings.\nDarklings are made of pure dark matter, they fled their home together with a new leader to escape their former master.");
+			DisplayName.SetDefault("Void Matter");
+			Tooltip.SetDefault("The essence of Darklings.");
 		}
 
 		public override void SetDefaults() 
 		{
-			Item.width = 20;
-			Item.height = 18;
+			Item.width = 34;
+			Item.height = 26;
 			Item.maxStack = 9999;
 			Item.value = Item.sellPrice(copper: 50);
 			Item.rare = ItemRarityID.Orange;
