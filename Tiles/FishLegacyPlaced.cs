9d404cb0eab97b8868d565e7cf7a818f9003613d
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-11-13 22:22:09 +0100

    the tile

---
+++ Tiles/FishLegacyPlaced.cs
@@ -0,0 +1,38 @@
+﻿using MultidimensionMod.Items.Placeables;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace MultidimensionMod.Tiles
+{
+	public class FishLegacyPlaced : ModTile
+	{
+		public override void SetStaticDefaults()
+		{
+			Main.tileFrameImportant[Type] = true;
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3Wall);
+			TileObjectData.newTile.Height = 5;
+			TileObjectData.newTile.Width = 5;
+			TileObjectData.newTile.CoordinateHeights = new[] { 16, 16, 16, 16, 16 };
+			TileID.Sets.DisableSmartCursor[Type] = true;
+			TileObjectData.addTile(Type);
+			ModTranslation name = CreateMapEntryName();
+			name.SetDefault("Fish Cleaver Display");
+			AddMapEntry(new Color(159, 115, 87), name);
+			DustType = 0;
+		}
+
+		public override void NumDust(int i, int j, bool fail, ref int num)
+		{
+			num = 5;
+		}
+
+		public override void KillMultiTile(int i, int j, int frameX, int frameY)
+		{
+			Item.NewItem(new EntitySource_TileBreak(i, j), i * 16, j * 16, 64, 32, ModContent.ItemType<FishLegacy>());
+		}
+	}
+}
