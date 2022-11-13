e7ec3e33227e69b0cf2985e93988959a5db05c8d
DoriraDimension <86755929+DoriraDimension@users.noreply.github.com>
2022-11-13 22:04:39 +0100

    Darkling AI (still need to get desktop git)

--- NPCs/Dungeon/Darkling.cs
+++ NPCs/Dungeon/Darkling.cs
@@ -1,15 +1,20 @@
 ﻿using MultidimensionMod.Items.Placeables.Banners;
 using MultidimensionMod.Items.Materials;
+using System;
+using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader.Utilities;
 using Terraria.GameContent.ItemDropRules;
 using Terraria.ModLoader;
+using Terraria.Audio;
 
 namespace MultidimensionMod.NPCs.Dungeon
 {
 	public class Darkling : ModNPC
 	{
+		public int Shootsies;
+
 		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Darkling");
@@ -18,7 +23,6 @@ namespace MultidimensionMod.NPCs.Dungeon
 
 		public override void SetDefaults()
 		{
-			NPC.CloneDefaults(NPCID.CursedSkull);
 			NPC.width = 46;
 			NPC.height = 46;
 			NPC.damage = 45;
@@ -28,10 +32,60 @@ namespace MultidimensionMod.NPCs.Dungeon
 			NPC.DeathSound = SoundID.NPCDeath52;
 			NPC.value = Item.buyPrice(0, 0, 1, 40);
 			NPC.knockBackResist = 0.6f;
-			NPC.aiStyle = 10;
-			AIType = NPCID.CursedSkull;
+			NPC.lavaImmune = true;
+			NPC.noGravity = true;
+			NPC.noTileCollide = true;
+			NPC.aiStyle = -1;
 			Banner = NPC.type;
 			BannerItem = ModContent.ItemType<DarklingBanner>();
+		}
+
+		public override void AI()
+        {
+			NPC.TargetClosest(true);
+			NPC.netUpdate = true;
+			Player player = Main.player[NPC.target];
+
+			Vector2 vector8 = new(NPC.position.X + (NPC.width * 0.5f), NPC.position.Y + (NPC.height * 0.5f));
+			{
+				float rotation = (float)Math.Atan2(vector8.Y - (Main.player[NPC.target].position.Y + (Main.player[NPC.target].height * 0.5f)), vector8.X - (Main.player[NPC.target].position.X + (Main.player[NPC.target].width * 0.5f)));
+				NPC.velocity.X = (float)(Math.Cos(rotation) * 4) * -1;
+				NPC.velocity.Y = (float)(Math.Sin(rotation) * 4) * -1;
+			}
+			NPC.rotation = NPC.velocity.ToRotation() + MathHelper.Pi;
+
+			float distanceToPlayer = Vector2.Distance(player.Center, NPC.Center);
+			if (distanceToPlayer < 150) //stops the NPC from moving if it is close to the player
+            {
+				NPC.velocity.X = 0;
+				NPC.velocity.Y = 0;
+            }
+			if (NPC.life < NPC.lifeMax) //Executes this code only if the enemy lost HP
+            {
+				Shootsies++;
+				if (Shootsies >= 200)
+				{
+					SoundEngine.PlaySound(SoundID.DD2_SonicBoomBladeSlash with { Volume = 0.4f }, NPC.position);
+					Vector2 velocity = Vector2.Normalize(player.Center - NPC.Center) * 10f;
+					Projectile.NewProjectile(NPC.GetSource_FromAI(), NPC.Center, velocity, ModContent.ProjectileType<DarklingShot>(), (int)((double)((float)NPC.damage) * 1.5), 0f, Main.myPlayer);
+					Shootsies = 0;
+				}
+			}
+
+			if (Main.dayTime && !player.ZoneDungeon) //despawns the enemy during daytime unless the player is inside the dungeon
+			{
+				SoundEngine.PlaySound(SoundID.NPCDeath6 with { Volume = .2f }, NPC.position);
+				NPC.active = false;
+				for (int i = 0; i < 5; i++) //spawns half the amount of dust than when the enemy gets hit
+				{
+					int dustType = Main.rand.Next(27, 27);
+					int dustIndex = Dust.NewDust(NPC.position, NPC.width, NPC.height, dustType);
+					Dust dust = Main.dust[dustIndex];
+					dust.velocity.X = dust.velocity.X + Main.rand.Next(-50, 51) * 0.01f;
+					dust.velocity.Y = dust.velocity.Y + Main.rand.Next(-50, 51) * 0.01f;
+					dust.scale *= 1f + Main.rand.Next(-30, 31) * 0.01f;
+				}
+			}
 		}
 
 		public override float SpawnChance(NPCSpawnInfo spawnInfo)
---
+++ NPCs/Dungeon/DarklingShot.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.Audio;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.NPCs.Dungeon
+{
+	internal class DarklingShot : ModProjectile
+	{
+		public override string Texture => "MultidimensionMod/Projectiles/NoTexture";
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Void Bolt");
+			Main.projFrames[Projectile.type] = 4;
+		}
+
+		public override void SetDefaults()
+		{
+			Projectile.width = 14;
+			Projectile.height = 14;
+			Projectile.hostile = true;
+			Projectile.ignoreWater = true;
+			Projectile.tileCollide = false;
+		}
+
+		public override void Kill(int timeLeft)
+		{
+			SoundEngine.PlaySound(SoundID.Item8, Projectile.position);
+
+			for (int i = 0; i < 50; i++)
+			{
+				int dustIndex = Dust.NewDust(new Vector2(Projectile.position.X, Projectile.position.Y), Projectile.width, Projectile.height, DustID.Shadowflame, 0f, 0f, 100, default(Color), 2f);
+				Main.dust[dustIndex].velocity *= 1.4f;
+				int dustIndex2 = Dust.NewDust(new Vector2(Projectile.position.X, Projectile.position.Y), Projectile.width, Projectile.height, DustID.Frost, 0f, 0f, 100, default(Color), 2f);
+				Main.dust[dustIndex2].velocity *= 1.4f;
+			}
+		}
+
+		public override void AI()
+		{
+			for (int i = 0; i < 4; i++)
+			{
+				int purple = Dust.NewDust(new Vector2(Projectile.position.X, Projectile.position.Y), Projectile.width, Projectile.height, DustID.Shadowflame, Projectile.velocity.X * 0.30f, Projectile.velocity.Y * 0.30f, 200, default(Color), 3f);
+				Main.dust[purple].noGravity = true;
+				Main.dust[purple].velocity.X *= 0.15f;
+				Main.dust[purple].velocity.Y *= 0.15f;
+			}
+			for (int i = 0; i < 2; i++)
+            {
+				int blue = Dust.NewDust(new Vector2(Projectile.position.X, Projectile.position.Y), Projectile.width, Projectile.height, DustID.Frost, Projectile.velocity.X * 0.30f, Projectile.velocity.Y * 0.30f, 200, default(Color), 3f);
+				Main.dust[blue].noGravity = true;
+				Main.dust[blue].velocity.X *= 0.15f;
+				Main.dust[blue].velocity.Y *= 0.15f;
+			}
+
+			Projectile.direction = Projectile.spriteDirection = Projectile.velocity.X > 0f ? 1 : -1;
+			Projectile.rotation = Projectile.velocity.ToRotation();
+			if (Projectile.velocity.Y > 16f)
+			{
+				Projectile.velocity.Y = 16f;
+			}
+			if (Projectile.spriteDirection == -1)
+			{
+				Projectile.rotation += MathHelper.Pi;
+			}
+		}
+	}
+}
