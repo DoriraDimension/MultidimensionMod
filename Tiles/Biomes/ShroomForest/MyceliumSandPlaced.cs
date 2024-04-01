985614be4f6b4941d27137eb24edf01277b608b3
Dorirā <thehackedphantom@gmail.com>
2024-04-01 17:02:38 +0200

    Mycelium Sand update
    Mycelium Sand now uses the new tmodloader feature of how to spawn sand balls.

--- Tiles/Biomes/ShroomForest/MyceliumSandPlaced.cs
+++ Tiles/Biomes/ShroomForest/MyceliumSandPlaced.cs
@@ -37,87 +37,29 @@ namespace MultidimensionMod.Tiles.Biomes.ShroomForest
             TileID.Sets.Conversion.Sand[Type] = true;
             TileID.Sets.ForAdvancedCollision.ForSandshark[Type] = true;
             TileID.Sets.Falling[Type] = true;
+            TileID.Sets.FallingBlockProjectile[Type] = new TileID.Sets.FallingBlockProjectileInfo(ModContent.ProjectileType<MyceliumSandBall>(), 10);
             TileID.Sets.CanBeClearedDuringOreRunner[Type] = true;
+            TileID.Sets.CanBeDugByShovel[Type] = true;
+            TileID.Sets.GeneralPlacementTiles[Type] = false;
+            TileID.Sets.ChecksForMerge[Type] = true;
             Main.tileBlockLight[Type] = true;
             AddMapEntry(new Color(195, 125, 56));
             DustType = DustID.Sand;
         }
-        public override void FloorVisuals(Player player)
-        {
-            if (player.velocity.X != 0f && Main.rand.NextBool(20))
-            {
-                Dust dust = Dust.NewDustDirect(player.Bottom, 0, 0, DustType, 0f, -Main.rand.NextFloat(2f));
-                dust.noGravity = true;
-                dust.fadeIn = 1f;
-            }
-        }
-        public override void KillTile(int i, int j, ref bool fail, ref bool effectOnly, ref bool noItem)
-        {
 
+        public override bool HasWalkDust()
+        {
+            return Main.rand.NextBool(3);
         }
+
+        public override void WalkDust(ref int dustType, ref bool makeDust, ref Color color)
+        {
+            dustType = DustID.Sand;
+        }
+
         public override void NumDust(int i, int j, bool fail, ref int num)
         {
             num = fail ? 1 : 3;
-        }
-        public override bool TileFrame(int i, int j, ref bool resetFrame, ref bool noBreak)
-        {
-            if (WorldGen.noTileActions)
-                return true;
-
-            Tile above = Main.tile[i, j - 1];
-            Tile below = Main.tile[i, j + 1];
-            bool canFall = true;
-
-            if (below == null || below.HasTile)
-                canFall = false;
-
-            if (above.HasTile && (TileID.Sets.BasicChest[above.TileType] || TileID.Sets.BasicChestFake[above.TileType] || above.TileType == TileID.PalmTree))
-                canFall = false;
-
-            if (canFall)
-            {
-                int projectileType = ModContent.ProjectileType<MyceliumSandBall>();
-                float positionX = i * 16 + 8;
-                float positionY = j * 16 + 8;
-
-                if (Main.netMode == NetmodeID.SinglePlayer)
-                {
-                    Main.tile[i, j].ClearTile();
-                    int proj = Projectile.NewProjectile(new EntitySource_TileBreak(i, j), positionX, positionY, 0f, 0.41f, projectileType, 10, 0f, Main.myPlayer);
-                    Main.projectile[proj].ai[0] = 1f;
-                    WorldGen.SquareTileFrame(i, j);
-                }
-                else if (Main.netMode == NetmodeID.Server)
-                {
-                    Tile Mtile = Main.tile[i, j];
-                    Mtile.HasTile = false;
-                    bool spawnProj = true;
-
-                    for (int k = 0; k < 1000; k++)
-                    {
-                        Projectile otherProj = Main.projectile[k];
-
-                        if (otherProj.active && otherProj.owner == Main.myPlayer && otherProj.type == projectileType && Math.Abs(otherProj.timeLeft - 3600) < 60 && otherProj.Distance(new Vector2(positionX, positionY)) < 4f)
-                        {
-                            spawnProj = false;
-                            break;
-                        }
-                    }
-
-                    if (spawnProj)
-                    {
-                        int proj = Projectile.NewProjectile(new EntitySource_TileBreak(i, j), positionX, positionY, 0f, 2.5f, projectileType, 10, 0f, Main.myPlayer);
-                        Main.projectile[proj].velocity.Y = 0.5f;
-                        Main.projectile[proj].position.Y += 2f;
-                        Main.projectile[proj].netUpdate = true;
-                    }
-
-                    NetMessage.SendTileSquare(-1, i, j, 1);
-                    WorldGen.SquareTileFrame(i, j);
-                }
-                return false;
-            }
-            return true;
         }
 
         public override void RandomUpdate(int i, int j)
