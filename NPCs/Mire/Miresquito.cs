0f77f50c4bbe028a4e7bd2e7535372ae40f7cb9d
Dorirā <thehackedphantom@gmail.com>
2024-08-06 22:43:03 +0200

    Miresquito sprite & AI changes (also Veilmud)
    -Gave Veilmud an actual sprite.
    -Gave Miresquito an actual sprite.
    -Miresquito creates a drink sound on player hits and gains weight when consuming blood, slowing it down. After hitting the player 6 times, they become immobile and lose their previous OnHit abilities.

--- NPCs/Mire/Miresquito.cs
+++ NPCs/Mire/Miresquito.cs
@@ -1,7 +1,10 @@
+using Microsoft.Xna.Framework;
 using MultidimensionMod.Base;
 using MultidimensionMod.Biomes;
 using Terraria;
+using Terraria.Audio;
 using Terraria.GameContent.Bestiary;
+using Terraria.GameContent.ItemDropRules;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -13,7 +16,7 @@ namespace MultidimensionMod.NPCs.Mire
 		public override void SetStaticDefaults()
 		{
 			//DisplayName.SetDefault("Miresquito");
-			Main.npcFrameCount[NPC.type] = 4;
+			Main.npcFrameCount[NPC.type] = 16;
 		}
 
 		public override void SetDefaults()
@@ -23,7 +26,7 @@ namespace MultidimensionMod.NPCs.Mire
             NPC.noTileCollide = false;
             NPC.width = 64;
 			NPC.height = 64;
-			NPC.damage = 45;
+			NPC.damage = 25;
 			NPC.defense = 10;
 			NPC.lifeMax = 200;
             NPC.HitSound = SoundID.NPCHit1;
@@ -33,6 +36,8 @@ namespace MultidimensionMod.NPCs.Mire
             NPC.knockBackResist = 0.5f;
             SpawnModBiomes = new int[1] { ModContent.GetInstance<TheShroudedMire>().Type };
         }
+
+        public int TheNefariousObeseness = 0;
 
         public override void SetBestiary(BestiaryDatabase dataNPC, BestiaryEntry bestiaryEntry)
         {
@@ -44,20 +49,78 @@ namespace MultidimensionMod.NPCs.Mire
 
         public override void FindFrame(int frameHeight)
         {
-            if (NPC.frameCounter++ > 7)
+            if (TheNefariousObeseness < 2)
             {
-                NPC.frame.Y += 60;
-                NPC.frameCounter = 0;
-                if (NPC.frame.Y >= 240)
+                if (NPC.frameCounter++ > 3)
                 {
-                    NPC.frame.Y = 0;
+                    NPC.frame.Y += 64;
+                    NPC.frameCounter = 0;
+                    if (NPC.frame.Y >= 192)
+                    {
+                        NPC.frame.Y = 0;
+                    }
+                }
+            }
+            if (TheNefariousObeseness >= 2 && TheNefariousObeseness < 4)
+            {
+                if (NPC.frameCounter++ > 4)
+                {
+                    NPC.frame.Y += 64;
+                    NPC.frameCounter = 0;
+                    if (NPC.frame.Y >= 512)
+                    {
+                        NPC.frame.Y = 192;
+                    }
+                }
+            }
+            if (TheNefariousObeseness >= 4 && TheNefariousObeseness < 6)
+            {
+                if (NPC.frameCounter++ > 5)
+                {
+                    NPC.frame.Y += 64;
+                    NPC.frameCounter = 0;
+                    if (NPC.frame.Y >= 766)
+                    {
+                        NPC.frame.Y = 512;
+                    }
+                }
+            }
+            if (TheNefariousObeseness >= 6)
+            {
+                if (NPC.frameCounter++ > 7)
+                {
+                    NPC.frame.Y += 64;
+                    NPC.frameCounter = 0;
+                    if (NPC.frame.Y >= 960)
+                    {
+                        NPC.frame.Y = 766;
+                    }
                 }
             }
         }
 
         public override void AI()
         {
-            BaseAI.AIFlier(NPC, ref NPC.ai, false, 0.2f, 0.1f, 3, 2.5f, true, 250);
+            int maxVelocity = 4;
+            if (TheNefariousObeseness >= 2 && TheNefariousObeseness < 4)
+            {
+                maxVelocity = (int)2.5;
+            }
+            else if (TheNefariousObeseness >= 4 && TheNefariousObeseness < 6)
+            {
+                maxVelocity = 1;
+            }
+            else if (TheNefariousObeseness >= 6)
+            {
+                maxVelocity = 0;
+                NPC.noGravity = false;
+                NPC.velocity.X = 0;
+                NPC.aiStyle = -1;
+            }
+            if (TheNefariousObeseness < 6)
+            {
+                BaseAI.AIFlier(NPC, ref NPC.ai, false, 0.2f, 0.1f, maxVelocity, 2.5f, true, 250);
+            }
             NPC.rotation = NPC.velocity.X * 0.05f;
             if (NPC.velocity.X > 0)
             {
@@ -71,7 +134,72 @@ namespace MultidimensionMod.NPCs.Mire
 
         public override void OnHitPlayer(Player target, Player.HurtInfo hurtInfo)
         {
-            NPC.life += 5;
+            if (TheNefariousObeseness < 6)
+            {
+                SoundEngine.PlaySound(SoundID.Item3, NPC.position);
+                TheNefariousObeseness++;
+                NPC.life += 5;
+            }
         }
-	}
+
+        //Drop hearts on death when fat enough (At least tier 3)
+        public override void ModifyNPCLoot(NPCLoot NPCloot)
+        {
+            if (TheNefariousObeseness >= 6)
+            {
+                NPCloot.Add(ItemDropRule.Common(ItemID.Heart, 1, 4, 4));
+            }
+            else if (TheNefariousObeseness >= 6)
+            {
+                NPCloot.Add(ItemDropRule.Common(ItemID.Heart, 1, 2, 2));
+            }
+        }
+
+        public override void HitEffect(NPC.HitInfo hit)
+        {
+            if (NPC.life <= 0)
+            {
+                Gore.NewGore(NPC.GetSource_FromThis(), NPC.position, NPC.velocity, ModContent.Find<ModGore>("MultidimensionMod/MiresquitoGore1").Type, 1); //head
+                Gore.NewGore(NPC.GetSource_FromThis(), NPC.position, NPC.velocity, ModContent.Find<ModGore>("MultidimensionMod/MiresquitoGore2").Type, 1); //wing
+                //Rest of the body has no gores, instead it creates a blood explosion that inscreases
+                if (TheNefariousObeseness < 2)
+                {
+                    int dust = DustID.Blood;
+                    Dust.NewDust(new Vector2(NPC.position.X, NPC.position.Y), NPC.width, NPC.height, dust, 0f, 0f, 0);
+                    Dust.NewDust(new Vector2(NPC.position.X, NPC.position.Y), NPC.width, NPC.height, dust, 0f, 0f, 0);
+                    Dust.NewDust(new Vector2(NPC.position.X, NPC.position.Y), NPC.width, NPC.height, dust, 0f, 0f, 0);
+                }
+                if (TheNefariousObeseness >= 2 && TheNefariousObeseness < 4)
+                {
+                    for (int i = 0; i < 16; i++)
+                    {
+                        int dust = DustID.Blood;
+                        Dust.NewDust(new Vector2(NPC.position.X, NPC.position.Y), NPC.width, NPC.height, dust, 0f, 0f, 0);
+                        Dust.NewDust(new Vector2(NPC.position.X, NPC.position.Y), NPC.width, NPC.height, dust, 0f, 0f, 0);
+                        Dust.NewDust(new Vector2(NPC.position.X, NPC.position.Y), NPC.width, NPC.height, dust, 0f, 0f, 0);
+                    }
+                }
+                if (TheNefariousObeseness >= 4 && TheNefariousObeseness < 6)
+                {
+                    for (int i = 0; i < 32; i++)
+                    {
+                        int dust = DustID.Blood;
+                        Dust.NewDust(new Vector2(NPC.position.X, NPC.position.Y), NPC.width * (int)1.2, NPC.height * (int)1.2, dust, 0f, 0f, 0);
+                        Dust.NewDust(new Vector2(NPC.position.X, NPC.position.Y), NPC.width * (int)1.2, NPC.height * (int)1.2, dust, 0f, 0f, 0);
+                        Dust.NewDust(new Vector2(NPC.position.X, NPC.position.Y), NPC.width * (int)1.2, NPC.height * (int)1.2, dust, 0f, 0f, 0);
+                    }
+                }
+                if (TheNefariousObeseness >= 6)
+                {
+                    for (int i = 0; i < 64; i++)
+                    {
+                        int dust = DustID.Blood;
+                        Dust.NewDust(new Vector2(NPC.position.X, NPC.position.Y), NPC.width * (int)1.5, NPC.height * (int)1.5, dust, 0f, 0f, 0);
+                        Dust.NewDust(new Vector2(NPC.position.X, NPC.position.Y), NPC.width * (int)1.5, NPC.height * (int)1.5, dust, 0f, 0f, 0);
+                        Dust.NewDust(new Vector2(NPC.position.X, NPC.position.Y), NPC.width * (int)1.5, NPC.height * (int)1.5, dust, 0f, 0f, 0);
+                    }
+                }
+            }
+        }
+    }
 }
