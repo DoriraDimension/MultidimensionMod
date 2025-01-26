f18984e9387f4f7e31dc8a90b489318f917ad9da
Dorirā <thehackedphantom@gmail.com>
2025-01-27 00:52:54 +0100

    Lantern improvements
    -Has proper name and tooltip now.
    -Now causes the player to emit some light
    -Moved to accessory folder

---
+++ Items/Accessories/FogLantern.cs
@@ -0,0 +1,49 @@
+﻿using MultidimensionMod.Base;
+using MultidimensionMod.Common.Players;
+using MultidimensionMod.Items.Materials;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.Graphics.Effects;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MultidimensionMod.Items.Accessories
+{
+    public class FogLantern : ModItem
+    {
+
+        public override void SetStaticDefaults()
+        {
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 33;
+            Item.height = 42;
+            Item.accessory = true;
+            Item.value = 0;
+            Item.rare = ItemRarityID.Blue;
+        }
+
+        public override void UpdateEquip(Player player)
+        {
+            player.AL().FogLantern = true;
+            Lighting.AddLight((int)(player.Center.X / 16f), (int)(player.Center.Y / 16f), 0.32f, 0.14f, 0.0f);
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+            .AddIngredient(ModContent.ItemType<DragonScale>(), 8)
+            .AddRecipeGroup(RecipeGroupID.IronBar, 12)
+            .AddTile(TileID.Anvils)
+            .Register();
+        }
+    }
+}
