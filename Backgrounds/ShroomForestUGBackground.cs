29a59a882d11acba5883afc5c8f36dedee513e67
Dorirā <thehackedphantom@gmail.com>
2025-02-27 01:56:52 +0100

    New mush menu theme + cavern bg

--- Backgrounds/ShroomForestUGBackground.cs
+++ Backgrounds/ShroomForestUGBackground.cs
@@ -8,8 +8,8 @@ namespace MultidimensionMod.Backgrounds
         {
             textureSlots[0] = BackgroundTextureLoader.GetBackgroundSlot("MultidimensionMod/Backgrounds/ShroomForestUGBGTransition");
             textureSlots[1] = BackgroundTextureLoader.GetBackgroundSlot("MultidimensionMod/Backgrounds/ShroomForestUGBG");
-            textureSlots[2] = BackgroundTextureLoader.GetBackgroundSlot("MultidimensionMod/Backgrounds/ShroomForestUGBGTransition");
-            textureSlots[3] = BackgroundTextureLoader.GetBackgroundSlot("MultidimensionMod/Backgrounds/ShroomForestUGBG");
+            textureSlots[2] = BackgroundTextureLoader.GetBackgroundSlot("MultidimensionMod/Backgrounds/ShroomForestCBGTransition");
+            textureSlots[3] = BackgroundTextureLoader.GetBackgroundSlot("MultidimensionMod/Backgrounds/ShroomForestCBG");
         }
     }
 }
