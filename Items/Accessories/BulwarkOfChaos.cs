e87e2bf2b17c084c16598846fcb0cd9215babf30
Dorirā <thehackedphantom@gmail.com>
2025-06-18 22:07:15 +0200

    Fixed Clutched Bulwark dash not existing

--- Items/Accessories/BulwarkOfChaos.cs
+++ Items/Accessories/BulwarkOfChaos.cs
@@ -30,6 +30,7 @@ Allows you to dash into enemies, damaging them");*/
         {
 			player.GetModPlayer<MDPlayer>().chaosClaw = true;
             player.dash = 2;
+            player.dashType = 2;
         }
         public override void AddRecipes()
         {
